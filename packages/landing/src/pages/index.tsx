6dd1103e4a5638d6438164c605510d67d4061c84
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2021-03-10 12:41:49 +0000

    Initial Commit

---
+++ packages/landing/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+const Home = (): JSX.Element => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <h1>Landing</h1>
+    </div>
+  );
+};
+
+export default Home;
