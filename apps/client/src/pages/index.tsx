9efcb8bc794e536eb7489daebbb0650134fa8d03
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2021-03-17 22:46:26 +0000

    feat: login page

--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -1,13 +1,23 @@
-import { Container } from '@chakra-ui/layout';
+import { Button } from '@chakra-ui/button';
+import { Box } from '@chakra-ui/layout';
 import { DeciEditor } from '@decipad/editor';
+import { signOut, useSession } from 'next-auth/client';
 import React from 'react';
 import { v4 } from 'uuid';
+import { Login } from '../components/Login/Login';
 
 const Home = () => {
+  const [session] = useSession();
   return (
-    <Container py="30px">
-      <DeciEditor docId={v4()} />
-    </Container>
+    <>
+      {!session && <Login />}
+      {session && (
+        <Box>
+          <Button onClick={() => signOut()}>Sign out</Button>
+          <DeciEditor docId={v4()} />
+        </Box>
+      )}
+    </>
   );
 };
 
