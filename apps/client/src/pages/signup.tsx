9243c0e5e5851eedf3ac0ca1a1bec9b914d9e724
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2021-04-15 10:38:14 +0100

    feat: signup ui components

--- apps/client/src/pages/signup.tsx
+++ apps/client/src/pages/signup.tsx
@@ -1,6 +1,15 @@
 import { Button } from '@chakra-ui/button';
 import { Input } from '@chakra-ui/input';
-import { Box, Center, Grid, Heading, HStack, Text } from '@chakra-ui/layout';
+import {
+  Box,
+  Center,
+  Grid,
+  Heading,
+  HStack,
+  Link,
+  Text,
+} from '@chakra-ui/layout';
+import { SlideFade } from '@chakra-ui/transition';
 import { Container } from '@decipad/ui';
 import Image from 'next/image';
 import React, { useState } from 'react';
@@ -8,10 +17,12 @@ import { TermsOfServiceNotice } from '../components/SignUp/TermsOfServiceNotice'
 
 const SignupPage = () => {
   const [value, setValue] = useState('');
+  const [codeScreen, setCodeScreen] = useState(false);
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log(value);
+    setCodeScreen(true);
   };
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
@@ -22,7 +33,7 @@ const SignupPage = () => {
   return (
     <Container h="100vh" py={10}>
       <Grid gridTemplateRows="auto 1fr auto" h="100%">
-        <Box pt={20}>
+        <Box pt={150}>
           <Center>
             <Box textAlign="center">
               <Center pb={5}>
@@ -36,40 +47,61 @@ const SignupPage = () => {
                   <Heading>Deci</Heading>
                 </HStack>
               </Center>
-              <Heading fontSize="4xl">Make sense of numbers.</Heading>
-              <Text opacity={0.6} pt={1} fontSize="20px">
-                Start using Deci by entering your email address.
-              </Text>
-              <form onSubmit={onSubmit} autoComplete="off">
-                <Input
-                  borderRadius="0"
-                  mt={10}
-                  name="email"
-                  focusBorderColor="pink.400"
-                  type="email"
-                  placeholder="name@company.com"
-                  onChange={onChange}
-                />
-                <Button
-                  type="submit"
-                  disabled={isDisabled}
-                  _disabled={{
-                    bg: 'blackAlpha.200',
-                    color: 'blackAlpha.400',
-                    cursor: 'not-allowed',
-                  }}
-                  w="100%"
-                  bg="pink.400"
-                  color="#fff"
-                  borderRadius="0"
-                  _hover={{
-                    bg: isDisabled ? 'blackAlpha.300' : 'pink.600',
-                  }}
-                  mt={5}
-                >
-                  Get Started
-                </Button>
-              </form>
+              {!codeScreen && (
+                <SlideFade in={!codeScreen} offsetY="20px" unmountOnExit>
+                  <Heading fontSize="4xl">Make sense of numbers.</Heading>
+                  <Text opacity={0.6} pt={1} fontSize="20px">
+                    Start using Deci by entering your email address.
+                  </Text>
+                  <form onSubmit={onSubmit} autoComplete="off">
+                    <Input
+                      borderRadius="0"
+                      mt={5}
+                      name="email"
+                      focusBorderColor="pink.400"
+                      type="email"
+                      placeholder="name@company.com"
+                      onChange={onChange}
+                    />
+                    <Button
+                      type="submit"
+                      disabled={isDisabled}
+                      _disabled={{
+                        bg: 'blackAlpha.200',
+                        color: 'blackAlpha.400',
+                        cursor: 'not-allowed',
+                      }}
+                      w="100%"
+                      bg="pink.400"
+                      color="#fff"
+                      borderRadius="0"
+                      _hover={{
+                        bg: isDisabled ? 'blackAlpha.300' : 'pink.600',
+                      }}
+                      mt={5}
+                    >
+                      Get Started
+                    </Button>
+                  </form>
+                </SlideFade>
+              )}
+              {codeScreen && (
+                <SlideFade in={codeScreen} offsetY="20px" unmountOnExit>
+                  <Box pt={5}>
+                    <Heading fontSize="3xl">Check your email address!</Heading>
+                    <Text pt={3} maxW="45ch" opacity={0.6}>
+                      We have sent you an email with a link that will allow you
+                      to sign in to your account immediately!
+                    </Text>
+                    <Text pt={3} color="pink.400">
+                      Didn't receive an email?{' '}
+                      <Link textDecor="underline" fontWeight="bold">
+                        Resend
+                      </Link>
+                    </Text>
+                  </Box>
+                </SlideFade>
+              )}
             </Box>
           </Center>
         </Box>
