d7977321c645d59c37a924ec2234eec3074e2524
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2021-04-22 11:58:54 +0100

    fix: results ui

--- libs/editor/src/Elements/CodeBlock.component.tsx
+++ libs/editor/src/Elements/CodeBlock.component.tsx
@@ -1,15 +1,31 @@
-import { Code, useColorModeValue } from '@chakra-ui/react';
+import { Box, Code, useColorModeValue } from '@chakra-ui/react';
 import React from 'react';
 import { RenderElementProps } from 'slate-react';
 
 export const CodeBlock = ({
   attributes,
   children,
+  element,
 }: RenderElementProps): JSX.Element => {
   const bg = useColorModeValue('gray.100', 'gray.700');
   return (
-    <Code w="100%" p="5" mb="3" mt="5" {...attributes} bg={bg} borderRadius="0">
-      {children}
-    </Code>
+    <Box>
+      {element.result && (
+        <Box contentEditable={false} bg="green.500" color="#fff">
+          {element.result}
+        </Box>
+      )}
+      <Code
+        w="100%"
+        p="5"
+        mb="3"
+        mt="5"
+        {...attributes}
+        bg={bg}
+        borderRadius="0"
+      >
+        {children}
+      </Code>
+    </Box>
   );
 };
