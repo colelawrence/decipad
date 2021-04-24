c13f25ba039dd7fe716e2d674c30d489f80b80a3
Pedro Teixeira <i@pgte.me>
2021-04-24 15:23:04 +0100

    chore: runtime: adds runtime session test

---
+++ libs/runtime/src/runtime.spec.ts
@@ -0,0 +1,49 @@
+import { nanoid } from 'nanoid';
+import { DeciRuntime } from './';
+
+const USER_ID = 'test user id';
+const ACTOR_ID = 'test actor id';
+
+describe('runtime', () => {
+  test('supports session', () => {
+    const deci = new DeciRuntime(USER_ID, ACTOR_ID);
+
+    let lastSession;
+    const sub = deci.getSession().subscribe((session) => {
+      lastSession = session
+    })
+
+    expect(deci.getSessionValue()).toBeNull();
+
+    deci.setSession({
+      user: {
+        id: 'id',
+        name: 'name',
+        email: 'email@example.com',
+        avatar: 'avatar'
+      }
+    });
+
+    expect(deci.getSessionValue()).toMatchObject({
+      user: {
+        id: 'id',
+        name: 'name',
+        email: 'email@example.com',
+        avatar: 'avatar'
+      }
+    });
+
+    expect(lastSession).toMatchObject({
+      user: {
+        id: 'id',
+        name: 'name',
+        email: 'email@example.com',
+        avatar: 'avatar'
+      }
+    });
+
+    sub.unsubscribe();
+    deci.stop();
+  });
+
+});
