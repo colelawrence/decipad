a6ad374eb4d172ee3e6207377fed4b732a58721a
Pedro Teixeira <i@pgte.me>
2021-05-07 18:54:38 +0100

    fix: desperately trying for arc to not touch that json env var

--- scripts/push-sam.sh
+++ scripts/push-sam.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-aws s3 cp --no-progress apps/backend/sam.yaml "${SAM_LOCATION}/"
-aws s3 cp --no-progress apps/backend/sam.json "${SAM_LOCATION}/"
+aws s3 cp --no-progress apps/backend/sam.yaml "${SAM_LOCATION}/sam.yaml"
+aws s3 cp --no-progress apps/backend/sam.json "${SAM_LOCATION}/sam.json"
