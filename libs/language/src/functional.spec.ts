dcae6e24b295995fa03d86ba18cc266afdb699f6
Fábio Santos <fabiosantosart@gmail.com>
2021-05-11 16:20:31 +0100

    fix basic percentages

--- libs/language/src/functional.spec.ts
+++ libs/language/src/functional.spec.ts
@@ -20,26 +20,26 @@ describe('use of funds document', () => {
   it('Can MVP the use of funds document', async () => {
     expect(
       await runCode(`
-      InitialInvestment = 300000
-      IncomeTax = 0.2
+        InitialInvestment = 300000
+        IncomeTax = 20%
 
-      costtobusiness = Month Salary StartDate Bonus => (
-        if dategte Month StartDate
-          then Salary + (Salary * 0.2) + (if Bonus then Salary * 0.2 else 0)
-          else 0
-      )
+        costtobusiness = Month Salary StartDate Bonus => (
+          if dategte Month StartDate
+            then Salary + (Salary * 20%) + (if Bonus then Salary * 20% else 0)
+            else 0
+        )
 
-      Months = [2021-01, 2021-02, 2021-03, 2021-04, 2021-05, 2021-06, 2021-07, 2021-08, 2021-09, 2021-10, 2021-11, 2021-12]
-      StandardSalary = 120000 / 12
+        Months = [2021-01, 2021-02, 2021-03, 2021-04, 2021-05, 2021-06, 2021-07, 2021-08, 2021-09, 2021-10, 2021-11, 2021-12]
+        StandardSalary = 120000 / 12
 
-      SalaryStaff = {
-        Months,
-        Exec = (costtobusiness Months StandardSalary 2021-01 true),
-        Product = (costtobusiness Months StandardSalary 2021-02 true),
-        Tech = (costtobusiness Months StandardSalary 2021-03 false),
-        FrontEnd = (costtobusiness Months StandardSalary 2021-03 true)
-      }
-    `)
+        SalaryStaff = {
+          Months,
+          Exec = (costtobusiness Months StandardSalary 2021-01 true),
+          Product = (costtobusiness Months StandardSalary 2021-02 true),
+          Tech = (costtobusiness Months StandardSalary 2021-03 false),
+          FrontEnd = (costtobusiness Months StandardSalary 2021-03 true)
+        }
+      `)
     ).toMatchObject({
       type: objectToTupleType({
         Months: Type.extend(Type.buildDate('month'), { columnSize: 12 }),
@@ -64,50 +64,50 @@ describe('use of funds document', () => {
     expect(
       await runCodeForVariables(
         `
-        InitialInvestment = 300000
-        IncomeTax = 0.2
+          InitialInvestment = 300000
+          IncomeTax = 20%
 
-        Salaries = {
-          Title = ["Exec", "Product", "Tech"]
-          Salary = [120000, 80000, 80000]
-          Department = ["G&A", "R&D", "R&D"]
-          StartDate = [2021-02, 2021-01, 2021-03]
-          Bonus = [false, true, false]
-        }
+          Salaries = {
+            Title = ["Exec", "Product", "Tech"]
+            Salary = [120000, 80000, 80000]
+            Department = ["G&A", "R&D", "R&D"]
+            StartDate = [2021-02, 2021-01, 2021-03]
+            Bonus = [false, true, false]
+          }
 
-        costtobusiness = Month Salary StartDate GetsBonus =>
-          if dategte Month StartDate
-            then Salary + (Salary * 0.2) + (if GetsBonus then Salary * 0.3 else 0)
-            else 0
+          costtobusiness = Month Salary StartDate GetsBonus =>
+            if dategte Month StartDate
+              then Salary + (Salary * 20%) + (if GetsBonus then Salary * 30% else 0)
+              else 0
 
-        Months = [2021-01, 2021-02, 2021-03, 2021-04]
+          Months = [2021-01, 2021-02, 2021-03, 2021-04]
 
-        StaffCosts = {
-          Title = Salaries.Title,
-          Salary = Salaries.Salary,
-          Costs = given Salaries: given Months:
-            costtobusiness Months (Salaries.Salary / 12) Salaries.StartDate Salaries.Bonus
-        }
+          StaffCosts = {
+            Title = Salaries.Title,
+            Salary = Salaries.Salary,
+            Costs = given Salaries: given Months:
+              costtobusiness Months (Salaries.Salary / 12) Salaries.StartDate Salaries.Bonus
+          }
 
-        TotalsPerMonth = total (transpose StaffCosts.Costs)
+          TotalsPerMonth = total (transpose StaffCosts.Costs)
 
-        isworking = Month StartDate => (dategte Month StartDate)
+          isworking = Month StartDate => (dategte Month StartDate)
 
-        countworking = Month StartDate =>
-          (total (given StartDate: if (isworking Month StartDate) then 1 else 0))
+          countworking = Month StartDate =>
+            (total (given StartDate: if (isworking Month StartDate) then 1 else 0))
 
-        HeadCountPerMonth = given Months: (countworking Months Salaries.StartDate)
+          HeadCountPerMonth = given Months: (countworking Months Salaries.StartDate)
 
-        HeadCountStepGrowth = stepgrowth HeadCountPerMonth
+          HeadCountStepGrowth = stepgrowth HeadCountPerMonth
 
-        Overheads = {
-          OtherCosts = 50 * HeadCountPerMonth,
-          NewHireCosts = 2000 * HeadCountStepGrowth,
-          FixedCosts = grow 2000 (0.05) Months
-        }
+          Overheads = {
+            OtherCosts = 50 * HeadCountPerMonth,
+            NewHireCosts = 2000 * HeadCountStepGrowth,
+            FixedCosts = grow 2000 (5%) Months
+          }
 
-        TotalOverheads = Overheads.OtherCosts + Overheads.NewHireCosts + Overheads.FixedCosts
-      `,
+          TotalOverheads = Overheads.OtherCosts + Overheads.NewHireCosts + Overheads.FixedCosts
+        `,
         [
           'StaffCosts',
           'TotalsPerMonth',
