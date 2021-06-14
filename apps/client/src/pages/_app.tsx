11d9881ebfabafcbcc8c750b4ee97787830284df
Ahmed Tarek <ahmedtarek@macbook-pro.mynet>
2021-06-14 12:06:29 +0100

    workspaces

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { ApolloProvider } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { RuntimeProvider } from '@decipad/editor';
 import { theme } from '@decipad/ui';
@@ -5,19 +6,23 @@ import { Provider } from 'next-auth/client';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import React from 'react';
+import { useApollo } from '../lib/apolloClient';
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const apolloClient = useApollo(pageProps);
   return (
     <>
       <Head>
         <title>Decipad</title>
       </Head>
       <Provider session={pageProps.session}>
-        <ChakraProvider resetCSS theme={theme}>
-          <RuntimeProvider>
-            <Component {...pageProps} />
-          </RuntimeProvider>
-        </ChakraProvider>
+        <ApolloProvider client={apolloClient}>
+          <ChakraProvider resetCSS theme={theme}>
+            <RuntimeProvider>
+              <Component {...pageProps} />
+            </RuntimeProvider>
+          </ChakraProvider>
+        </ApolloProvider>
       </Provider>
     </>
   );
