48786ad278ff7fc62363ea551b83fd70f372ef6c
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2021-06-18 18:51:30 +0100

    feat: workspaces deletion and renaming

---
+++ libs/queries/src/lib/operations/mutations/RENAME_WORKSPACE.ts
@@ -0,0 +1,29 @@
+import { gql, useMutation } from '@apollo/client';
+
+export const RENAME_WORKSPACE = gql`
+  mutation RenameWorkspace($id: ID!, $name: String!) {
+    updateWorkspace(id: $id, workspace: { name: $name }) {
+      id
+      name
+    }
+  }
+`;
+
+export interface RenameWorkspace {
+  updateWorkspace: {
+    id: string;
+    name: string;
+  };
+}
+
+export interface RenameWorkspaceVars {
+  id: string;
+  name: string;
+}
+
+export const useRenameWorkspace = ({ id, name }: RenameWorkspaceVars) =>
+  useMutation<RenameWorkspace, RenameWorkspaceVars>(RENAME_WORKSPACE, {
+    variables: { id, name },
+    refetchQueries: ['GetWorkspaceById'],
+    awaitRefetchQueries: true,
+  });
