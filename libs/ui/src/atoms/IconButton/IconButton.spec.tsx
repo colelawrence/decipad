69f21ebd3ef326e180ec1eedafcbd9602614e661
Tim Seckinger <seckinger.tim@gmail.com>
2021-08-03 20:13:43 +0200

    test(icon-button): fix console warning (#305)

--- libs/ui/src/atoms/IconButton/IconButton.spec.tsx
+++ libs/ui/src/atoms/IconButton/IconButton.spec.tsx
@@ -1,5 +1,5 @@
 import userEvent from '@testing-library/user-event';
-import { render } from 'test-utils';
+import { render } from '@testing-library/react';
 
 import { IconButton } from './IconButton';
 
