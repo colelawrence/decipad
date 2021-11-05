44b68fe08bc48896616107ac706ca6a4d46236ed
Pedro Teixeira <i@pgte.me>
2021-11-05 09:41:10 +0000

    push and pull sam

--- scripts/push-sam.sh
+++ scripts/push-sam.sh
@@ -2,12 +2,14 @@
 
 set -euo pipefail
 
-FILE="${SAM_LOCATION}/sam.yaml"
-if [ -f "${FILE}" ]; then
-  aws s3 cp --no-progress apps/backend/sam.yaml "${FILE}"
+REMOTE_FILE="${SAM_LOCATION}/sam.yaml"
+LOCAL_FILE=apps/backend/sam.yaml
+if [ -f "${LOCAL_FILE}" ]; then
+  aws s3 cp --no-progress "${LOCAL_FILE}" "${REMOTE_FILE}"
 fi
 
-FILE="${SAM_LOCATION}/sam.json"
-if [ -f "${FILE}" ]; then
-  aws s3 cp --no-progress apps/backend/sam.yaml "${FILE}"
+REMOTE_FILE="${SAM_LOCATION}/sam.json"
+LOCAL_FILE=apps/backend/sam.JSON
+if [ -f "${LOCAL_FILE}" ]; then
+  aws s3 cp --no-progress "${LOCAL_FILE}" "${REMOTE_FILE}"
 fi
