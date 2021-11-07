d574c56e28982d5918b4ae0163856209f057bb5e
Pedro Teixeira <i@pgte.me>
2021-11-07 13:16:00 +0000

    alpha deploy: hosted zone and certificate

--- apps/backend/src/plugins/custom-domain/index.js
+++ apps/backend/src/plugins/custom-domain/index.js
@@ -1,11 +1,8 @@
-// These values will be hard-coded for now so that we don't depend on flaky env vars
-// There are no secrets in these, so it's ok, you can relax.
-const CertificateArn = 'arn:aws:acm:eu-west-2:974894071214:certificate/98cb3526-6405-4341-80d1-6695b096ab05';
-const HostedZoneId = 'Z03939041TD6X5Z4MAXDZ';
-
 function package({ arc, cloudformation, stage='staging', inventory, createFunction }) {
 
   const customDomain = process.env.DECI_CUSTOM_DOMAIN;
+  const CertificateArn = process.env.DECI_CERTIFICATE_ARN;
+  const HostedZoneId = process.env.DECI_ZONE_ID;
   console.log(`Custom domain: stage = ${stage}, custom domain = ${customDomain}`);
   if (stage === 'staging' && customDomain) {
     console.log(`Configuring domain name in deploy: ${customDomain}`);
