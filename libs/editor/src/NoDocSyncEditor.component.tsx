1604e9b0243e6b8085c1f37a84077846b4557484
Pedro Teixeira <i@pgte.me>
2021-11-08 18:06:29 +0000

    fix: playground editor: can now do calculations (#495)

--- libs/editor/src/NoDocSyncEditor.component.tsx
+++ libs/editor/src/NoDocSyncEditor.component.tsx
@@ -1,7 +1,16 @@
-import { Plate, PlateProps } from '@udecode/plate';
-import { FC, useMemo } from 'react';
+import { nanoid } from 'nanoid';
+import { Plate, PlateProps, useStoreEditorRef } from '@udecode/plate';
+import { FC, useMemo, useEffect, useState } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import {
+  ProgramBlocksContextProvider,
+  ResultsContextProvider,
+} from '@decipad/ui';
+import {
+  editorProgramBlocks,
+  useLanguagePlugin,
+} from './plugins/Language/useLanguagePlugin';
 import { Tooltip } from './components';
 import { components, options, plugins } from './configuration';
 import {
@@ -10,26 +19,59 @@ import {
 } from './plugins/SlashCommands';
 
 export const NoDocSyncEditorBase = (props: PlateProps): ReturnType<FC> => {
+  const [editorId] = useState(nanoid);
+
+  const editor = useStoreEditorRef(editorId);
+  useEffect(() => {
+    if (editor) {
+      editor.children = [
+        {
+          type: 'h1',
+          children: [
+            {
+              text: '',
+            },
+          ],
+        },
+        {
+          type: 'p',
+          children: [
+            {
+              text: '',
+            },
+          ],
+        },
+      ];
+    }
+  }, [editor]);
+
+  const { results, languagePlugin } = useLanguagePlugin();
+  const programBlocks = editor ? editorProgramBlocks(editor) : {};
+
   const { getSlashCommandsProps, plugin: slashCommandsPlugin } =
     useSlashCommandsPlugin();
 
   const editorPlugins = useMemo(
-    () => [...plugins, slashCommandsPlugin],
-    [slashCommandsPlugin]
+    () => [...plugins, slashCommandsPlugin, languagePlugin],
+    [slashCommandsPlugin, languagePlugin]
   );
 
   return (
-    <Plate
-      id="no-doc-sync"
-      plugins={editorPlugins}
-      options={options}
-      components={components}
-      editableProps={{ autoFocus: true }}
-      {...props}
-    >
-      <Tooltip />
-      <SlashCommandsSelect {...getSlashCommandsProps()} />
-    </Plate>
+    <ResultsContextProvider value={results}>
+      <ProgramBlocksContextProvider value={programBlocks}>
+        <Plate
+          id={editorId}
+          plugins={editorPlugins}
+          options={options}
+          components={components}
+          editableProps={{ autoFocus: true }}
+          {...props}
+        >
+          <Tooltip />
+          <SlashCommandsSelect {...getSlashCommandsProps()} />
+        </Plate>
+      </ProgramBlocksContextProvider>
+    </ResultsContextProvider>
   );
 };
 
