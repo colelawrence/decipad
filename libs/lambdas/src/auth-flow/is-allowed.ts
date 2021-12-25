b650016b56bb4c3b6c5e6a80015a62f543e1b8a8
Pedro Teixeira <i@pgte.me>
2021-12-25 13:34:04 +0000

    fix: allow login if the user already has an account (#693)
    * allow login if the user already has an account
    * only require allow list for dev or alpha

--- libs/lambdas/src/auth-flow/is-allowed.ts
+++ libs/lambdas/src/auth-flow/is-allowed.ts
@@ -1,14 +1,25 @@
 import tables from '@decipad/services/tables';
 
-export async function isAllowedToLogIn(email?: string): Promise<boolean> {
+export async function isAllowedToLogIn(_email?: string): Promise<boolean> {
   if (process.env.NODE_ENV === 'testing') {
     return true;
   }
-  if (!email) {
+  if (!_email) {
     return false;
   }
+  const urlBase = process.env.DECI_APP_URL_BASE;
+  if (
+    !urlBase ||
+    (urlBase !== 'https://alpha.dev.decipad.com' &&
+      urlBase !== 'https://dev.decipad.com')
+  ) {
+    return true;
+  }
   const data = await tables();
-  return (
-    (await data.allowlist.get({ email: email.toLocaleLowerCase() })) != null
-  );
+  const email = _email.toLowerCase();
+  const emailKey = await data.userkeys.get({ id: `email:${email}` });
+  if (emailKey != null) {
+    return true;
+  }
+  return (await data.allowlist.get({ id: email })) != null;
 }
