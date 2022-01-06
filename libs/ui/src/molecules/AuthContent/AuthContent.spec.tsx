2764a334585d6e63672080fa94ba0e92d2925ca7
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-01-06 11:02:43 +0000

    improved authcontent spec file

--- libs/ui/src/molecules/AuthContent/AuthContent.spec.tsx
+++ libs/ui/src/molecules/AuthContent/AuthContent.spec.tsx
@@ -1,19 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { AuthContent } from './AuthContent';
 
 describe('Auth Content Molecule', () => {
   it('renders the title and description', () => {
     const { getByText } = render(
-      <AuthContent title="my title" description="this is a description" />
+      <AuthContent title="My Title" description="This is a description" />
     );
 
-    expect(getByText(/my title/i)).toBeInTheDocument();
-    expect(screen.getByText(/this is a description/i)).toBeInTheDocument();
+    expect(getByText('My Title')).toBeInTheDocument();
+    expect(getByText('This is a description')).toBeInTheDocument();
   });
 
   it('renders the deci brand svg', () => {
     const { getByTitle } = render(
-      <AuthContent title="my title" description="this is a description" />
+      <AuthContent title="My Title" description="This is a description" />
     );
 
     expect(getByTitle(/deci logo/i)).toBeInTheDocument();
