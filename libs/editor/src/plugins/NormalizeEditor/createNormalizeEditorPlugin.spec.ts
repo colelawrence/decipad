colelawrence/decipad

commit 12764c42c6266666f3df06a5bf645db44781c040

fix(editor): forbid code lines at top level (#877)
And other elements not on the allow list.
Does not deal with non-top-level elements yet.

libs/editor/src/plugins/NormalizeEditor/createNormalizeEditorPlugin.ts
@@ -1,0 +1,95 @@
+/* eslint-disable no-param-reassign */
+import {
+  getPlatePluginWithOverrides,
+  insertNodes,
+  TNode,
+  WithOverride,
+  wrapNodes,
+} from '@udecode/plate';
+import { Editor, Element, NodeEntry, Text, Transforms } from 'slate';
+import {
+  ELEMENT_BLOCKQUOTE,
+  ELEMENT_CODE_BLOCK,
+  ELEMENT_FETCH,
+  ELEMENT_H1,
+  ELEMENT_H2,
+  ELEMENT_H3,
+  ELEMENT_OL,
+  ELEMENT_PARAGRAPH,
+  ELEMENT_TABLE_INPUT,
+  ELEMENT_UL,
+} from '../../elements';
+
+const WithNormalizeEditor = (): WithOverride => (editor) => {
+  const { normalizeNode } = editor;
+
+  editor.normalizeNode = (entry) => {
+    const [node, path] = entry as NodeEntry<TNode>;
+
+    if (path.length === 1) {
+      if (path[0] === 0) {
+        // Enforce leading H1
+        if (Text.isText(node)) {
+          wrapNodes(editor, { type: ELEMENT_H1, children: [] }, { at: path });
+          return;
+        }
+        if (Element.isElement(node) && node.type !== ELEMENT_H1) {
+          Transforms.unwrapNodes(editor, { at: path });
+          return;
+        }
+      } else if (Element.isElement(node) && node.type === ELEMENT_H1) {
+        // Forbid H1s elsewhere
+        Transforms.unwrapNodes(editor, { at: path });
+        return;
+      }
+    }
+    // Enforce leading H1 even if there are no elements
+    if (Editor.isEditor(node)) {
+      if (!node.children.length) {
+        insertNodes(
+          editor,
+          { type: ELEMENT_H1, children: [] },
+          { at: [...path, 0] }
+        );
+        return;
+      }
+    }
+
+    // Enforce the top-level block allowed elements
+    if (path.length === 1) {
+      if (Text.isText(node)) {
+        wrapNodes(
+          editor,
+          { type: ELEMENT_PARAGRAPH, children: [] },
+          { at: path }
+        );
+        return;
+      }
+      if (
+        Element.isElement(node) &&
+        ![
+          ELEMENT_H1,
+          ELEMENT_H2,
+          ELEMENT_H3,
+          ELEMENT_PARAGRAPH,
+          ELEMENT_BLOCKQUOTE,
+          ELEMENT_CODE_BLOCK,
+          ELEMENT_UL,
+          ELEMENT_OL,
+          ELEMENT_TABLE_INPUT,
+          ELEMENT_FETCH,
+        ].includes(node.type)
+      ) {
+        Transforms.unwrapNodes(editor, { at: path });
+        return;
+      }
+    }
+
+    return normalizeNode([node, path]);
+  };
+
+  return editor;
+};
+
+export const createNormalizeEditorPlugin =
+  getPlatePluginWithOverrides(WithNormalizeEditor);

libs/editor/src/plugins/NormalizeEditor/createNormalizeEditorPlugin.spec.ts
@@ -1,0 +1,99 @@
+import { createEditorPlugins, TDescendant, TElement } from '@udecode/plate';
+import { Editor } from 'slate';
+import { createNormalizeEditorPlugin } from './createNormalizeEditorPlugin';
+import {
+  ELEMENT_H1,
+  ELEMENT_H2,
+  ELEMENT_BLOCKQUOTE,
+  ELEMENT_CODE_LINE,
+  ELEMENT_LI,
+  ELEMENT_LINK,
+  ELEMENT_PARAGRAPH,
+  ELEMENT_UL,
+  H1Element,
+} from '../../elements';
+
+const h1Element = (): H1Element => ({
+  type: ELEMENT_H1,
+  children: [{ text: '' }],
+});
+
+let editor: Editor;
+beforeEach(() => {
+  editor = createEditorPlugins({
+    plugins: [createNormalizeEditorPlugin()],
+  });
+});
+
+describe('the title normalization', () => {
+  it('forces the first block to be an H1', () => {
+    editor.children = [
+      {
+        type: ELEMENT_H2,
+        children: [{ text: '' }],
+      } as TElement,
+    ];
+    Editor.normalize(editor, { force: true });
+    expect(editor.children).toEqual([h1Element()]);
+  });
+  it('forces the first H1 to exist', () => {
+    editor.children = [];
+    Editor.normalize(editor, { force: true });
+    expect(editor.children).toEqual([h1Element()]);
+  });
+  it('forbids H1s in the second to last elements and converts them to paragraphs', () => {
+    editor.children = [h1Element(), h1Element()] as TElement[];
+    Editor.normalize(editor, { force: true });
+    expect(editor.children).toEqual([
+      expect.anything(),
+      {
+        type: ELEMENT_PARAGRAPH,
+        children: [{ text: '' }],
+      },
+    ]);
+  });
+});
+
+it.each([ELEMENT_H2, ELEMENT_UL, ELEMENT_BLOCKQUOTE])(
+  'allows the %s element at top level',
+  (type) => {
+    editor.children = [
+      h1Element(),
+      {
+        type,
+        children: [{ text: '' }],
+      } as TDescendant,
+    ];
+    Editor.normalize(editor, { force: true });
+    expect(editor.children).toEqual([
+      expect.anything(),
+      { type, children: [{ text: '' }] },
+    ]);
+  }
+);
+it.each([ELEMENT_LI, ELEMENT_LINK, ELEMENT_CODE_LINE, 'asdf'])(
+  'converts the %s element forbidden at top level to a paragraph',
+  (type) => {
+    editor.children = [
+      h1Element(),
+      {
+        type,
+        children: [{ text: '' }],
+      } as TDescendant,
+    ];
+    Editor.normalize(editor, { force: true });
+    expect(editor.children).toEqual([
+      expect.anything(),
+      { type: ELEMENT_PARAGRAPH, children: [{ text: '' }] },
+    ]);
+  }
+);
+
+it('converts text at top level to a paragraph', () => {
+  editor.children = [h1Element(), { text: '' }];
+  Editor.normalize(editor, { force: true });
+  expect(editor.children).toEqual([
+    expect.anything(),
+    { type: ELEMENT_PARAGRAPH, children: [{ text: '' }] },
+  ]);
+});

libs/editor/src/configuration/plugins.ts
@@ -14,19 +14,19 @@ import {
   createTrailingBlockPlugin,
 } from '@udecode/plate';
 import { nanoid } from 'nanoid';
+import { ELEMENT_PARAGRAPH } from '../elements';
+import { autoformatRules } from './autoformat';
+import { exitBreakOptions } from './exitBreakOptions';
+import { resetBlockTypeOptions } from './resetBlockTypeOptions';
 import { createAutoPairsPlugin } from '../plugins/AutoPairs/createAutoPairsPlugin';
 import { createCodeVariableHighlightingPlugin } from '../plugins/CodeVariableHighlighting/createCodeVariableHighlightingPlugin';
-import { createForcedLayoutPlugin } from '../plugins/ForcedLayout/createForcedLayoutPlugin';
 import { createInteractiveTablePlugin } from '../plugins/InteractiveTable/createInteractiveTablePlugin';
 import { createSentryBreadcrumbsPlugin } from '../plugins/SentryBreadcrumbsPlugin/createSentryBreadcrumbsPlugin';
 import { createMarksPlugins } from '../plugins/Marks/createMarksPlugins';
 import { createLinkPlugin } from '../plugins/Link/createLinkPlugin';
 import { createNormalizeCodeBlockPlugin } from '../plugins/NormalizeCodeBlock/createNormalizeCodeBlockPlugin';
 import { createSoftBreakPlugin } from '../plugins/SoftBreakPlugin/createSoftBreakPlugin';
-import { autoformatRules } from './autoformat';
-import { exitBreakOptions } from './exitBreakOptions';
-import { resetBlockTypeOptions } from './resetBlockTypeOptions';
-import { ELEMENT_PARAGRAPH } from '../elements';
+import { createNormalizeEditorPlugin } from '../plugins/NormalizeEditor/createNormalizeEditorPlugin';
 
 export const plugins = [
   // fundamentals
@@ -50,7 +50,8 @@ export const plugins = [
   createCodeBlockPlugin(),
 
   // structure enforcement
-  createForcedLayoutPlugin(),
+  createNormalizeEditorPlugin(),
+  createNormalizeCodeBlockPlugin(),
   createTrailingBlockPlugin({ type: ELEMENT_PARAGRAPH }),
 
   // block manipulation
@@ -65,7 +66,6 @@ export const plugins = [
   createLinkPlugin(),
 
   // code editing
-  createNormalizeCodeBlockPlugin(),
   createCodeVariableHighlightingPlugin(),
   createAutoPairsPlugin(),
 ];

libs/editor/src/plugins/ForcedLayout/createForcedLayoutPlugin.ts
@@ -1,47 +1,0 @@
-/* eslint-disable no-param-reassign */
-// TODO fix node types
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import {
-  getNode,
-  getPlatePluginWithOverrides,
-  WithOverride,
-} from '@udecode/plate';
-import { Transforms } from 'slate';
-import { ELEMENT_H1, ELEMENT_PARAGRAPH } from '../../elements';
-
-export const WithForcedLayout = (): WithOverride => (editor) => {
-  const { normalizeNode } = editor;
-
-  editor.normalizeNode = ([currentNode, currentPath]) => {
-    if (!currentPath.length) {
-      const firstNode = getNode(editor, [0]);
-      const secondNode = getNode(editor, [1]);
-
-      // first node will always be an h1
-      if (firstNode && (firstNode as any).type !== ELEMENT_H1) {
-        const newNode = {
-          ...firstNode,
-          type: ELEMENT_H1,
-        };
-        Transforms.setNodes(editor, newNode, { at: [0] });
-      }
-
-      // if first node is an h1, change it to paragraph
-      // this makes it possible for other types like blockquotes to also be the second node
-      if (secondNode && (secondNode as any).type === ELEMENT_H1) {
-        const newNode = {
-          ...secondNode,
-          type: ELEMENT_PARAGRAPH,
-        };
-        Transforms.setNodes(editor, newNode, { at: [1] });
-      }
-    }
-
-    return normalizeNode([currentNode, currentPath]);
-  };
-
-  return editor;
-};
-
-export const createForcedLayoutPlugin =
-  getPlatePluginWithOverrides(WithForcedLayout);

commit bd1b50c5a3157b0a153328812f71b41c7bae2bd6

fix(editor): H1 duplication when pressing enter in the title
As I guessed, the problem was that the relevant nodes weren't actually
normalized at all because they were merely moved down by other nodes
appearing on top, but not changed themselves.
So it turns out editor-level normalization must actually be done to the
editor node, by traversing its children.

This looks like a complete rewrite esp. without diff
--ignore-all-space, but it's far from it 😅

The old behavior is restored. That behavior may be slightly confusing if
pressing enter at the start of the title. But even if we wanted to
change this — not in scope of this PR, which just fixes the bug
introduced by #877

libs/editor/src/plugins/NormalizeEditor/createNormalizeEditorPlugin.ts
@@ -2,11 +2,14 @@
 import {
   getPlatePluginWithOverrides,
   insertNodes,
+  isElement,
+  isText,
+  TDescendant,
   TNode,
   WithOverride,
   wrapNodes,
 } from '@udecode/plate';
-import { Editor, Element, NodeEntry, Text, Transforms } from 'slate';
+import { Node, NodeEntry, Transforms } from 'slate';
 import {
   ELEMENT_BLOCKQUOTE,
   ELEMENT_CODE_BLOCK,
@@ -26,25 +29,8 @@ const WithNormalizeEditor = (): WithOverride => (editor) => {
   editor.normalizeNode = (entry) => {
     const [node, path] = entry as NodeEntry<TNode>;
 
-    if (path.length === 1) {
-      if (path[0] === 0) {
-        // Enforce leading H1
-        if (Text.isText(node)) {
-          wrapNodes(editor, { type: ELEMENT_H1, children: [] }, { at: path });
-          return;
-        }
-        if (Element.isElement(node) && node.type !== ELEMENT_H1) {
-          Transforms.unwrapNodes(editor, { at: path });
-          return;
-        }
-      } else if (Element.isElement(node) && node.type === ELEMENT_H1) {
-        // Forbid H1s elsewhere
-        Transforms.unwrapNodes(editor, { at: path });
-        return;
-      }
-    }
-    // Enforce leading H1 even if there are no elements
-    if (Editor.isEditor(node)) {
+    if (!path.length) {
+      // Enforce leading H1 even if there are no elements
       if (!node.children.length) {
         insertNodes(
           editor,
@@ -53,35 +39,56 @@ const WithNormalizeEditor = (): WithOverride => (editor) => {
         );
         return;
       }
-    }
+      for (const blockEntry of Node.children(editor, path)) {
+        const [blockNode, blockPath] = blockEntry as NodeEntry<TDescendant>;
 
-    // Enforce the top-level block allowed elements
-    if (path.length === 1) {
-      if (Text.isText(node)) {
-        wrapNodes(
-          editor,
-          { type: ELEMENT_PARAGRAPH, children: [] },
-          { at: path }
-        );
-        return;
-      }
-      if (
-        Element.isElement(node) &&
-        ![
-          ELEMENT_H1,
-          ELEMENT_H2,
-          ELEMENT_H3,
-          ELEMENT_PARAGRAPH,
-          ELEMENT_BLOCKQUOTE,
-          ELEMENT_CODE_BLOCK,
-          ELEMENT_UL,
-          ELEMENT_OL,
-          ELEMENT_TABLE_INPUT,
-          ELEMENT_FETCH,
-        ].includes(node.type)
-      ) {
-        Transforms.unwrapNodes(editor, { at: path });
-        return;
+        if (blockPath[0] === 0) {
+          // Enforce leading H1
+          if (isText(blockNode)) {
+            wrapNodes(
+              editor,
+              { type: ELEMENT_H1, children: [] },
+              { at: blockPath }
+            );
+            return;
+          }
+          if (isElement(blockNode) && blockNode.type !== ELEMENT_H1) {
+            Transforms.unwrapNodes(editor, { at: blockPath });
+            return;
+          }
+        } else if (isElement(blockNode) && blockNode.type === ELEMENT_H1) {
+          // Forbid H1s elsewhere
+          Transforms.unwrapNodes(editor, { at: blockPath });
+          return;
+        }
+
+        // Enforce the top-level block allowed elements
+        if (isText(blockNode)) {
+          wrapNodes(
+            editor,
+            { type: ELEMENT_PARAGRAPH, children: [] },
+            { at: blockPath }
+          );
+          return;
+        }
+        if (
+          isElement(blockNode) &&
+          ![
+            ELEMENT_H1,
+            ELEMENT_H2,
+            ELEMENT_H3,
+            ELEMENT_PARAGRAPH,
+            ELEMENT_BLOCKQUOTE,
+            ELEMENT_CODE_BLOCK,
+            ELEMENT_UL,
+            ELEMENT_OL,
+            ELEMENT_TABLE_INPUT,
+            ELEMENT_FETCH,
+          ].includes(blockNode.type)
+        ) {
+          Transforms.unwrapNodes(editor, { at: blockPath });
+          return;
+        }
       }
     }
 

libs/editor/src/plugins/NormalizeEditor/createNormalizeEditorPlugin.spec.ts
@@ -1,5 +1,5 @@
 import { createEditorPlugins, TDescendant, TElement } from '@udecode/plate';
-import { Editor } from 'slate';
+import { Editor, Transforms } from 'slate';
 import { createNormalizeEditorPlugin } from './createNormalizeEditorPlugin';
 import {
   ELEMENT_H1,
@@ -49,6 +49,20 @@ describe('the title normalization', () => {
       {
         type: ELEMENT_PARAGRAPH,
         children: [{ text: '' }],
+      },
+    ]);
+  });
+  it('applies to moved but unchanged blocks', () => {
+    editor.children = [
+      { type: ELEMENT_H1, children: [{ text: 'text' }] },
+    ] as TElement[];
+    Transforms.select(editor, { path: [0, 0], offset: 0 });
+    editor.insertNode({ children: [{ text: '' }] });
+    expect(editor.children).toEqual([
+      h1Element(),
+      {
+        type: ELEMENT_PARAGRAPH,
+        children: [{ text: 'text' }],
       },
     ]);
   });