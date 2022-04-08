503505724aae51b3451f12c50232e4e1defef9da
Nuno Job <dscape@users.noreply.github.com>
2022-04-08 16:19:38 +0100

    Change headings in tooltip to match design system (#1044)

--- libs/ui/src/icons/Heading/Heading.tsx
+++ libs/ui/src/icons/Heading/Heading.tsx
@@ -3,14 +3,11 @@ import { cssVar } from '../../primitives';
 
 export const Heading = (): ReturnType<FC> => {
   return (
-    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
       <title>Heading</title>
       <path
-        d="M5.75 5.75H7.25M7.25 5.75H8.25M7.25 5.75V11.75M7.25 18.25H5.75M7.25 18.25H8.25M7.25 18.25V11.75M7.25 11.75H16.75M16.75 11.75V5.75M16.75 11.75V18.25M18.25 5.75H16.75M16.75 5.75H15.75M16.75 18.25H18.25M16.75 18.25H15.75"
-        stroke={cssVar('currentTextColor')}
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        d="M4.20312 13.0277V2.97229H5.42077V7.45009H10.7824V2.97229H12V13.0277H10.7824V8.53026H5.42077V13.0277H4.20312Z"
+        fill={cssVar('currentTextColor')}
       />
     </svg>
   );
