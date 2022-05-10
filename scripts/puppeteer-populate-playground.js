89c5af1e4b12f0465b074035e0d8aa7667834523
Nuno Job <dscape@users.noreply.github.com>
2022-05-10 22:50:34 +0100

    Update puppeteer-populate-playground.js

--- scripts/puppeteer-populate-playground.js
+++ scripts/puppeteer-populate-playground.js
@@ -1,14 +1,9 @@
-const puppeteer = require('puppeteer');
-
-const BASE_URL = process.env.LHCI_SERVER_BASE_URL;
-
-module.export = async () => {
-  const browser = await puppeteer.launch();
+module.exports = async (browser, context) => {
   const page = await browser.newPage();
-  await page.goto(BASE_URL);
+  await page.goto(process.env.LHCI_SERVER_BASE_URL);
   await page.evaluate(() => {
     localStorage.setItem('deciPopulatePlayground', 'true');
   });
 
-  await browser.close();
+  await page.close();
 };
