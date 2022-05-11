b89e156dc78d7717d3addac7945a8d1e07d10cfe
Nuno Job <dscape@users.noreply.github.com>
2022-05-11 10:18:27 +0100

    Update puppeteer-populate-playground.js

--- scripts/puppeteer-populate-playground.js
+++ scripts/puppeteer-populate-playground.js
@@ -1,6 +1,8 @@
 module.exports = async (browser, context) => {
   const page = await browser.newPage();
-  await page.goto(process.env.LHCI_SERVER_BASE_URL);
+  // main commits dont have PR
+  const url = process.env.LHCI_SERVER_BASE_URL === 'https://.dev.decipad.com' ? 'https://dev.decipad.com' : process.env.LHCI_SERVER_BASE_URL;
+  await page.goto(url);
   await page.evaluate(() => {
     localStorage.setItem('deciPopulatePlayground', 'true');
   });
