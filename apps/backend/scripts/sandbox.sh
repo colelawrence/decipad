e8001d966cb3644f007bacef449019865053d26b
Pedro Teixeira <i@pgte.me>
2022-05-26 10:41:36 +0100

    watch build (#1329)

--- apps/backend/scripts/sandbox.sh
+++ apps/backend/scripts/sandbox.sh
@@ -3,6 +3,19 @@
 set -euo pipefail
 
 cd ../..
-npm run build:backend
+
+SERVICE_PIDS=""
+
+teardown () {
+  if [ -n "$SERVICE_PIDS" ]; then
+    echo "Killing these PIDEs:" $SERVICE_PIDS
+    kill -s SIGINT -- $SERVICE_PIDS
+    true
+  fi
+}
+
+trap "teardown" EXIT
+yarn build:backend:watch&
+SERVICE_PIDS="${SERVICE_PIDS} ${!}"
 cd apps/backend
 NEXTAUTH_URL=http://localhost:4200 ../../node_modules/.bin/sandbox
