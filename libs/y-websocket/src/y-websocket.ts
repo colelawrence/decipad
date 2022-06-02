3a324b9672759a01c7b0533c68f35f8c5d6c1953
Kirill <xakdog@gmail.com>
2022-06-02 17:47:27 +0200

    Reading message noise (#1371)

--- libs/y-websocket/src/y-websocket.ts
+++ libs/y-websocket/src/y-websocket.ts
@@ -102,7 +102,7 @@ export const readMessage = (
   }
   try {
     // eslint-disable-next-line no-console
-    console.log('reading message', buf);
+    console.debug('reading message', buf);
     const message = Buffer.from(buf as string, 'base64');
     const decoder = decoding.createDecoder(message);
     const encoder = encoding.createEncoder();
