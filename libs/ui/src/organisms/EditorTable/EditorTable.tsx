e9bb26af573a3ae7e19ba351cab4ce71808f50e4
André Santos <andrefs@andrefs.com>
2022-06-23 16:58:56 +0100

    Quick fix auto complete menu on formula columns (#1470)

--- libs/ui/src/organisms/EditorTable/EditorTable.tsx
+++ libs/ui/src/organisms/EditorTable/EditorTable.tsx
@@ -41,7 +41,6 @@ const tableCaptionWrapperStyles = css({
   width: '100%',
   minWidth: editorLayout.slimBlockWidth,
   maxWidth: restWidthBlock,
-  overflowX: 'scroll',
   display: 'inline-block',
   [smallScreenQuery]: {
     maxWidth: `calc(100vw - ${gutterWidth})`,
