dadf0e4bd6db1567baf004d7a70a604d96acddaf
Pedro Teixeira <i@pgte.me>
2022-06-23 14:35:02 +0100

    set the secret as a token on the graphql client (#1466)

--- apps/frontend/src/meta/providers/GraphqlProvider.tsx
+++ apps/frontend/src/meta/providers/GraphqlProvider.tsx
@@ -1,19 +1,23 @@
 import { devtoolsExchange } from '@urql/devtools';
 import { cacheExchange } from '@urql/exchange-graphcache';
-import { FC, ReactNode } from 'react';
+import produce from 'immer';
+import { FC, ReactNode, useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
 import {
   createClient,
   dedupExchange,
   fetchExchange,
   Provider as UrqlProvider,
+  ClientOptions,
 } from 'urql';
 import { graphCacheConfig } from '../../graphql/cacheConfig';
 
-const client = createClient({
+const defaultClientOpts = {
   url: new URL(`/graphql`, window.location.origin).toString(),
-  fetchOptions: () => ({
+  fetchOptions: {
     credentials: 'same-origin',
-  }),
+    headers: {},
+  },
   suspense: true, // React Suspense
   exchanges: [
     devtoolsExchange,
@@ -21,12 +25,31 @@ const client = createClient({
     cacheExchange(graphCacheConfig),
     fetchExchange,
   ],
-});
+};
 
 interface GraphqlProviderProps {
   readonly children: ReactNode;
 }
 
 export const GraphqlProvider: FC<GraphqlProviderProps> = ({ children }) => {
+  const { search } = useLocation();
+  const clientOpts = useMemo(() => {
+    const params = new URLSearchParams(search);
+    const secret = params.get('secret');
+    return produce(defaultClientOpts, (opts) => {
+      if (secret) {
+        // eslint-disable-next-line no-param-reassign
+        opts.fetchOptions.headers = {
+          ...(opts.fetchOptions.headers || {}),
+          authorization: `Bearer ${secret}`,
+        };
+      }
+    });
+  }, [search]);
+
+  const client = useMemo(
+    () => createClient(clientOpts as ClientOptions),
+    [clientOpts]
+  );
   return <UrqlProvider value={client}>{children}</UrqlProvider>;
 };
