5ac01814e4b84e1922dd34f010fef04d2bef7752
Ziad Beyens <ziad.beyens@gmail.com>
2022-07-06 13:43:54 +0200

    fix (#1533)

--- libs/editor-table/src/plugins/withTable.spec.ts
+++ libs/editor-table/src/plugins/withTable.spec.ts
@@ -10,9 +10,11 @@ import {
   ELEMENT_PARAGRAPH,
   ELEMENT_TABLE,
   ELEMENT_TABLE_CAPTION,
+  ELEMENT_TABLE_VARIABLE_NAME,
   ELEMENT_TD,
   ELEMENT_TH,
   ELEMENT_TR,
+  TableElement,
 } from '@decipad/editor-types';
 import { createTablePlugin } from './createTablePlugin';
 
@@ -28,8 +30,8 @@ describe('withTable', () => {
     });
   });
 
-  describe('when inserting a table without caption', () => {
-    it('should add a caption', () => {
+  describe('when inserting a table without caption, with th row', () => {
+    it('should add empty caption', () => {
       editor.children = [
         {
           type: ELEMENT_PARAGRAPH,
@@ -46,6 +48,63 @@ describe('withTable', () => {
           id: '1',
           type: ELEMENT_TABLE,
           children: [
+            {
+              id: '1',
+              type: ELEMENT_TR,
+              children: [
+                {
+                  id: '1',
+                  type: ELEMENT_TH,
+                  children: [{ text: 'a' }],
+                  cellType: {
+                    kind: 'string',
+                  },
+                },
+              ],
+            },
+            {
+              id: '1',
+              type: ELEMENT_TR,
+              children: [
+                {
+                  id: '1',
+                  type: ELEMENT_TD,
+                  children: [{ text: '2' }],
+                },
+              ],
+            },
+          ],
+        },
+      ]);
+
+      expect(editor.children[0].children[0].type).toBe(ELEMENT_TABLE_CAPTION);
+      expect(
+        (editor.children[0] as TableElement).children[1].children[0].children[0]
+          .text
+      ).toBe('a');
+      expect(
+        (editor.children[0] as TableElement).children[2].children[0].children[0]
+          .text
+      ).toBe('2');
+    });
+  });
+
+  describe('when inserting a table without caption into a table', () => {
+    it('should insert cells', () => {
+      editor.children = [
+        {
+          id: '1',
+          type: ELEMENT_TABLE,
+          children: [
+            {
+              type: ELEMENT_TABLE_CAPTION,
+              children: [
+                {
+                  type: ELEMENT_TABLE_VARIABLE_NAME,
+                  children: [{ text: '' }],
+                },
+              ],
+            },
             {
               id: '1',
               type: ELEMENT_TR,
@@ -73,9 +132,76 @@ describe('withTable', () => {
             },
           ],
         },
+      ] as never;
+      editor.selection = {
+        anchor: { path: [0, 2, 0, 0], offset: 0 },
+        focus: { path: [0, 2, 0, 0], offset: 0 },
+      };
+
+      editor.insertFragment([
+        {
+          id: '1',
+          type: ELEMENT_TABLE,
+          children: [
+            {
+              id: '1',
+              type: ELEMENT_TR,
+              children: [
+                {
+                  id: '1',
+                  type: ELEMENT_TD,
+                  children: [{ text: '1' }],
+                },
+              ],
+            },
+          ],
+        },
       ]);
 
-      expect(editor.children[0].children[0].type).toBe(ELEMENT_TABLE_CAPTION);
+      expect(
+        (editor.children[0] as TableElement).children[2].children[0].children[0]
+          .text
+      ).toBe('1');
+    });
+  });
+
+  describe('when inserting a table without caption, without th row', () => {
+    it('should add empty th row', () => {
+      editor.children = [
+        {
+          type: ELEMENT_PARAGRAPH,
+          children: [{ text: '' }],
+        },
+      ] as never;
+      editor.selection = {
+        anchor: { path: [0, 0], offset: 0 },
+        focus: { path: [0, 0], offset: 0 },
+      };
+
+      editor.insertFragment([
+        {
+          id: '1',
+          type: ELEMENT_TABLE,
+          children: [
+            {
+              id: '1',
+              type: ELEMENT_TR,
+              children: [
+                {
+                  id: '1',
+                  type: ELEMENT_TD,
+                  children: [{ text: '1' }],
+                },
+              ],
+            },
+          ],
+        },
+      ]);
+
+      expect(
+        (editor.children[0] as TableElement).children[2].children[0].children[0]
+          .text
+      ).toBe('1');
     });
   });
 });
--- libs/editor-table/src/plugins/withTable.ts
+++ libs/editor-table/src/plugins/withTable.ts
@@ -2,11 +2,27 @@
 import {
   ELEMENT_TABLE,
   ELEMENT_TABLE_CAPTION,
+  ELEMENT_TH,
+  ELEMENT_TR,
   getMyEditor,
+  MyElement,
   MyWithOverride,
+  TableHeaderRowElement,
 } from '@decipad/editor-types';
-import { isElement, withTable as withPlateTable } from '@udecode/plate';
+import {
+  isElement,
+  someNode,
+  withTable as withPlateTable,
+} from '@udecode/plate';
 import { createTableCaption } from '../utils/createTableCaption';
+
+const createEmptyTableHeaderCell = () => ({
+  type: ELEMENT_TH,
+  cellType: {
+    kind: 'string',
+  },
+  children: [{ text: '' }],
+});
 
 export const withTable: MyWithOverride = (editor, plugin) => {
   editor = withPlateTable(editor, plugin);
@@ -14,21 +30,45 @@ export const withTable: MyWithOverride = (editor, plugin) => {
   const myEditor = getMyEditor(editor);
   const { insertFragment } = myEditor;
 
+  /**
+   * If not in a table and inserted table has no caption:
+   * - add a table caption as first child
+   * - add an empty header row if first row is not composed of th
+   */
   myEditor.insertFragment = (fragment) => {
-    fragment = fragment.map((node) => {
-      if (isElement(node) && node.type === ELEMENT_TABLE) {
-        if (node.children[0]?.type !== ELEMENT_TABLE_CAPTION) {
-          node.children.unshift(
-            createTableCaption({
-              id: node.id,
-            })
-          );
-          return node;
-        }
-      }
+    if (!someNode(editor, { match: { type: ELEMENT_TABLE } })) {
+      fragment = fragment.map((node) => {
+        if (isElement(node) && node.type === ELEMENT_TABLE) {
+          if (!node.children.length) return node;
 
-      return node;
-    });
+          if (node.children[0].type !== ELEMENT_TABLE_CAPTION) {
+            const isHeaderRow = node.children[0].children.every(
+              (cell) => (cell as MyElement).type === ELEMENT_TH
+            );
+
+            if (!isHeaderRow) {
+              const cells = Array.from(
+                { length: node.children[0].children.length },
+                createEmptyTableHeaderCell
+              );
+
+              node.children.unshift({
+                type: ELEMENT_TR,
+                children: cells,
+              } as unknown as TableHeaderRowElement);
+            }
+
+            node.children.unshift(
+              createTableCaption({
+                id: node.id,
+              })
+            );
+          }
+        }
+
+        return node;
+      });
+    }
 
     insertFragment(fragment);
   };
