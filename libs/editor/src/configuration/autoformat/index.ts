7a4409644e732658c3dbc0be1db4477fd5050b63
Nuno Job <dscape@users.noreply.github.com>
2022-07-14 19:18:14 +0100

    ENG-969 (#1563)
    * fixes ENG-969
    
    * Update libs/editor/src/configuration/autoformat/index.ts
    
    Co-authored-by: Fábio Santos <fabiosantosart@gmail.com>
    
    * Update libs/editor/src/configuration/autoformat/index.ts
    
    Co-authored-by: Fábio Santos <fabiosantosart@gmail.com>
    
    * Update index.ts
    
    Co-authored-by: Fábio Santos <fabiosantosart@gmail.com>

--- libs/editor/src/configuration/autoformat/index.ts
+++ libs/editor/src/configuration/autoformat/index.ts
@@ -1,10 +1,42 @@
-import { MyAutoformatRule } from '@decipad/editor-types';
-import { autoformatArrow, autoformatPunctuation } from '@udecode/plate';
+import { ELEMENT_CODE_LINE, MyAutoformatRule } from '@decipad/editor-types';
+import {
+  autoformatArrow,
+  autoformatLegal,
+  autoformatLegalHtml,
+  autoformatMath,
+  autoformatPunctuation,
+  autoformatSmartQuotes,
+  someNode,
+  TEditor,
+} from '@udecode/plate';
 import { autoformatBlocks } from './autoformatBlocks';
 import { autoformatImages } from './autoformatImages';
 import { autoformatLinks } from './autoformatLinks';
 import { autoformatLists } from './autoformatLists';
 import { autoformatMarks } from './autoformatMarks';
+
+//
+// Prevent arrow rules to be applied in codelines, like <= becoming ⇐
+//
+function disableCodelineFormatting(editor: TEditor) {
+  return !someNode(editor, {
+    match: { type: ELEMENT_CODE_LINE },
+  });
+}
+
+const rulesForbiddenInCodeLines = [
+  autoformatArrow,
+  autoformatPunctuation,
+  autoformatLegal,
+  autoformatSmartQuotes,
+  autoformatLegalHtml,
+  autoformatMath,
+].flatMap((ruleSet) =>
+  ruleSet.map((rule) => ({
+    ...rule,
+    query: disableCodelineFormatting,
+  }))
+);
 
 export const autoformatRules = [
   ...autoformatBlocks,
@@ -12,6 +44,5 @@ export const autoformatRules = [
   ...autoformatMarks,
   ...autoformatLinks,
   ...autoformatImages,
-  ...autoformatPunctuation,
-  ...autoformatArrow,
+  ...rulesForbiddenInCodeLines,
 ] as MyAutoformatRule[];
