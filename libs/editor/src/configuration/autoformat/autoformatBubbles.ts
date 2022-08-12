aa067378be989ac82073638b531041de0e2951a6
Kirill <xakdog@gmail.com>
2022-08-12 13:57:26 +0200

    Make sure autoformat rules are not leaking to alpha (#1704)

--- libs/editor/src/configuration/autoformat/autoformatBubbles.ts
+++ libs/editor/src/configuration/autoformat/autoformatBubbles.ts
@@ -7,6 +7,7 @@ import {
 import { requireCollapsedSelection } from '@decipad/editor-utils';
 import { insertNodes, isText } from '@udecode/plate';
 import words from 'random-words';
+import { isEnabled } from '@decipad/feature-flags';
 import { doesSelectionAllowTextStyling } from './doesSelectionAllowTextStyling';
 
 const convertPrecedingTextWithTriggerToImage = (editor: MyEditor): void => {
@@ -26,13 +27,15 @@ const convertPrecedingTextWithTriggerToImage = (editor: MyEditor): void => {
   });
 };
 
-export const autoformatBubbles: MyAutoformatRule[] = [
-  {
-    mode: 'block',
-    type: ELEMENT_BUBBLE,
-    triggerAtBlockStart: false,
-    match: '+',
-    query: doesSelectionAllowTextStyling,
-    format: convertPrecedingTextWithTriggerToImage,
-  },
-];
+export const autoformatBubbles: MyAutoformatRule[] = isEnabled('INLINE_BUBBLES')
+  ? [
+      {
+        mode: 'block',
+        type: ELEMENT_BUBBLE,
+        triggerAtBlockStart: false,
+        match: '+',
+        query: doesSelectionAllowTextStyling,
+        format: convertPrecedingTextWithTriggerToImage,
+      },
+    ]
+  : [];
