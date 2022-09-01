4bf93af97fa6387d9c3e48f93f72731bf707a2f9
Fábio Santos <fabiosantosart@gmail.com>
2022-09-01 13:50:14 +0100

    catch a syntax error in a Variable Input (#1792)

--- libs/editor-language-elements/src/utils/inferExpression.ts
+++ libs/editor-language-elements/src/utils/inferExpression.ts
@@ -1,10 +1,17 @@
-import { parseOneBlock, isExpression } from '@decipad/computer';
+import { parseOneExpression } from '@decipad/computer';
 import { CoercibleType } from '../types';
 
 export const inferExpression = (text: string): CoercibleType | undefined => {
-  const block = parseOneBlock(text);
-  if (block.args.length === 1 && isExpression(block.args[0])) {
-    return { type: { kind: 'anything' }, coerced: text };
+  try {
+    const parsed = parseOneExpression(text);
+    if (parsed.type !== 'noop') {
+      return {
+        type: { kind: 'anything' },
+        coerced: text,
+      };
+    }
+  } catch {
+    // Parse error
   }
   return undefined;
 };
