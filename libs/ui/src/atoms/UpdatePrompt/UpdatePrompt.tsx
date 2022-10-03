3fd8120e37dfec5863f84abc43f80e7ce7ee4cbc
Pedro Teixeira <i@pgte.me>
2022-10-03 10:45:07 +0100

    prevent reload from goping home (#2160)

--- libs/ui/src/atoms/UpdatePrompt/UpdatePrompt.tsx
+++ libs/ui/src/atoms/UpdatePrompt/UpdatePrompt.tsx
@@ -1,13 +1,24 @@
+import { MouseEvent, useCallback } from 'react';
+
 interface UpdatePromptProps {
   onReload: () => void;
 }
 
 export const UpdatePrompt = ({ onReload }: UpdatePromptProps) => {
+  const onReloadClick = useCallback(
+    (event: MouseEvent) => {
+      event.preventDefault();
+      event.stopPropagation();
+      onReload();
+    },
+    [onReload]
+  );
+
   return (
     <div>
       <p>There is a new version of Deci available.</p>
       <p>
-        <a href="/" onClick={onReload}>
+        <a href="/" onClick={onReloadClick}>
           Click here to update
         </a>
       </p>
