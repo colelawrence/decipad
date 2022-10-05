fc6f618f2b2be838cd631146201966f6e57c04ce
Pedro Teixeira <i@pgte.me>
2022-10-05 13:35:47 +0100

    fraction parts can come in string format as well (bigints) (#2186)

--- libs/fraction/src/isFractionLike.ts
+++ libs/fraction/src/isFractionLike.ts
@@ -10,8 +10,9 @@ export const isFractionLike = (f: unknown): f is FractionLike => {
   return (
     typeof f === 'object' &&
     f !== null &&
-    fractionLikeProps.every(
-      (prop) => prop in f && typeof (f as FractionLike)[prop] === 'bigint'
-    )
+    fractionLikeProps.every((prop) => {
+      const tof = typeof (f as FractionLike)[prop];
+      return prop in f && (tof === 'bigint' || tof === 'string');
+    })
   );
 };
