5599f8539c5c1afa06a13f8f21ae7178b01549b2
Pedro Teixeira <i@pgte.me>
2022-11-03 11:23:44 +0000

    table first column is anything (#2402)

--- libs/editor-components/src/utils/table.ts
+++ libs/editor-components/src/utils/table.ts
@@ -37,7 +37,7 @@ const initialTableElement = () => {
           {
             id: nanoid(),
             type: ELEMENT_TH,
-            cellType: { kind: 'string' },
+            cellType: { kind: 'anything' },
             children: [{ text: 'Property1' }],
           },
           {
