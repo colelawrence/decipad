41255c395fd880ef1721cc5f0658cbe4b8088a06
Simao <SimaoDias@users.noreply.github.com>
2022-11-04 17:00:57 +0000

    Terms and conditions (#2436)
    * Terms and Condiations
    
    * little fix

---
+++ apps/docs/src/pages/terms.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import '../css/custom.css';
+
+export default function Terms() {
+  return (
+    <div
+      style={{
+        backgroundImage:
+          'radial-gradient(circle farthest-corner at 50% -30%, #fafcff, #f2f5f9 30%, #fff 90%)',
+      }}
+    >
+      <div
+        style={{
+          maxWidth: '800px',
+          margin: 'auto',
+          marginTop: '100px',
+          marginBottom: '120px',
+        }}
+      >
+        <div
+          style={{
+            margin: '20px',
+          }}
+        >
+          <div
+            style={{
+              marginBottom: '50px',
+            }}
+          >
+            <center>
+              <h1
+                class="title-gallery"
+                style={{
+                  fontFamily: 'Alliance, sans-serif',
+                  fontSize: '70px',
+                  lineHeight: '120%',
+                  fontWeight: '500',
+                }}
+              >
+                Terms and Conditions
+              </h1>
+              <p
+                style={{
+                  fontFamily: 'Inter, sans-serif',
+                  fontSize: '20px',
+                  color: '#5f6486',
+                }}
+              >
+                Laste Updated on November 4th 2022
+              </p>
+              <p>
+                See also <u>Privacy Policy</u> <u>Cookie Policy</u>{' '}
+                <u>Copyright Notice</u>
+              </p>
+            </center>
+          </div>
+          <p>
+            These terms and conditions (‘Terms’) apply to the Decipad service
+            (“Service”). Please read them carefully, as they contain important
+            information about the Service, the rules for using it, and the
+            limitation of our legal liability. You can print off these Terms or
+            store them for future reference. If you do not agree to these Terms,
+            you must not use the Service.
+          </p>
+          <br></br>
+
+          <h2>1. About Us</h2>
+
+          <p>
+            These terms and conditions (‘Terms’) apply to the Decipad service
+            (“Service”). Please read them carefully, as they contain important
+            information about the Service, the rules for using it, and the
+            limitation of our legal liability. You can print off these Terms or
+            store them for future reference. If you do not agree to these Terms,
+            you must not use the Service.
+          </p>
+
+          <h2>2. About Us</h2>
+
+          <p>
+            These terms and conditions (‘Terms’) apply to the Decipad service
+            (“Service”). Please read them carefully, as they contain important
+            information about the Service, the rules for using it, and the
+            limitation of our legal liability. You can print off these Terms or
+            store them for future reference. If you do not agree to these Terms,
+            you must not use the Service.
+          </p>
+
+          <br></br>
+
+          <hr></hr>
+          <br></br>
+          <center>
+            <br></br>
+            <p>
+              Have any questions? Send us an email at{' '}
+              <u>
+                <a href="mailto:support@decipad.com">support@decipad.com</a>
+              </u>{' '}
+              or write us at:
+            </p>
+          </center>
+        </div>
+      </div>
+    </div>
+  );
+}
