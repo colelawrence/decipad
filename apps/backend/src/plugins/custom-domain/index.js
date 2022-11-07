afe33b44781721360ce1a54ff36b2e8a7fd58253
Pedro Teixeira <i@pgte.me>
2022-11-07 14:55:58 +0000

    Revert "no more domain handling at the level of architect"
    This reverts commit ed4a9ed24ea040b28bd913da9ecbc3915f73b57f.

---
+++ apps/backend/src/plugins/custom-domain/index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  deploy: {
+    start: async ({ cloudformation, stage }) => {
+      if (stage !== 'staging') {
+        return cloudformation;
+      }
+      const customDomain = process.env.DECI_CUSTOM_DOMAIN;
+      const CertificateArn = process.env.DECI_CERTIFICATE_ARN;
+      const HostedZoneId = process.env.DECI_ZONE_ID;
+      console.log(`Custom domain: stage = ${stage}, custom domain = ${customDomain}`);
+      if (customDomain) {
+        console.log(`Configuring domain name in deploy: ${customDomain}`);
+        cloudformation.Resources.HTTP.Properties.Domain = {
+          DomainName: customDomain,
+          CertificateArn,
+          Route53: {
+            HostedZoneId,
+            DistributionDomainName: customDomain,
+          }
+        };
+      }
+
+      return cloudformation;
+    }
+  }
+};
