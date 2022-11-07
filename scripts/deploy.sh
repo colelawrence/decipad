5700dcc182ceb48a45e3a8b73a1cee547f41c64c
Pedro Teixeira <i@pgte.me>
2022-11-07 12:40:09 +0000

    deploy prod script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -36,8 +36,6 @@ yarn build:backend
 echo "Clearing sourcemaps..."
 find apps/backend/public/ | grep '\.map$' | xargs rm
 
-echo "Setting some very important env vars..."
-export DECI_APP_URL_BASE = 'https://app.decipad.com'
 
 echo "Deploying..."
 mkdir -p tmp/deploy
