529a78075799509600b4c213deff7f474b96471b
Kirill <xakdog@gmail.com>
2022-12-02 13:20:30 +0100

    Add error boundary for MediaEmbed (#2633)

--- libs/editor-components/src/Media/MediaEmbed.tsx
+++ libs/editor-components/src/Media/MediaEmbed.tsx
@@ -1,17 +1,22 @@
 import { MediaEmbed as UIMediaEmbed } from '@decipad/ui';
 import { ELEMENT_MEDIA_EMBED, PlateComponent } from '@decipad/editor-types';
 import { useIsEditorReadOnly } from '@decipad/react-contexts';
+import { assertElementType } from '@decipad/editor-utils';
 import { DraggableBlock } from '../block-management/index';
+import { BlockErrorBoundary } from '../BlockErrorBoundary';
 
 export const MediaEmbed: PlateComponent = (props) => {
+  assertElementType(props.element, ELEMENT_MEDIA_EMBED);
   const readOnly = useIsEditorReadOnly();
 
   return (
-    <UIMediaEmbed
-      draggableBlock={DraggableBlock}
-      readOnly={readOnly}
-      pluginKey={ELEMENT_MEDIA_EMBED}
-      {...props}
-    />
+    <BlockErrorBoundary element={props.element}>
+      <UIMediaEmbed
+        draggableBlock={DraggableBlock}
+        readOnly={readOnly}
+        pluginKey={ELEMENT_MEDIA_EMBED}
+        {...props}
+      />
+    </BlockErrorBoundary>
   );
 };
