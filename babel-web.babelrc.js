c7aec8524dd100c7bb3a75257a1c76096ae77d83
André Jonas <andre@n1n.co>
2023-01-05 10:52:46 +0000

    Makes classnames even more friendly and across all environments so it's easier for everyone to debug and find out where are the styles they want to change. This is also a good way to encourage anyone to make small styling contributions to the codebase, most of all the design team, who actually asked for this. (#2802)

--- babel-web.babelrc.js
+++ babel-web.babelrc.js
@@ -5,5 +5,16 @@ module.exports = {
       { runtime: 'automatic', importSource: '@emotion/react' },
     ],
   ],
-  plugins: [require.resolve('@emotion/babel-plugin')],
+  plugins: [
+    [
+      require.resolve('@emotion/babel-plugin'),
+      {
+        sourceMap: true,
+        // Always provides friendly classnames (according to `labelFormat`) for debugging purposes.
+        autoLabel: 'always',
+        labelFormat: '-[filename]--[local]',
+        cssPropOptimization: true,
+      },
+    ],
+  ],
 };
