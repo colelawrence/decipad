0a2fce31867ef7c1c0c3a002fca54571521a43f3
Pedro Teixeira <i@pgte.me>
2023-03-04 21:39:19 +0000

    fix

--- apps/frontend/src/workspaces/RedirectToDefaultWorkspace.tsx
+++ apps/frontend/src/workspaces/RedirectToDefaultWorkspace.tsx
@@ -1,10 +1,17 @@
 import { FC } from 'react';
 import { Navigate } from 'react-router-dom';
 import { workspaces } from '@decipad/routing';
+import { LoadingLogo } from '@decipad/ui';
 import { useGetWorkspacesIDsQuery } from '../graphql';
 
 const RedirectToDefaultWorkspace: FC = () => {
   const [results] = useGetWorkspacesIDsQuery();
+  if (results.error) {
+    throw results.error;
+  }
+  if (results.fetching) {
+    return <LoadingLogo />;
+  }
   const firstWorkspaceWithWritePermissions = results.data?.workspaces.filter(
     (ws) => ws.myPermissionType != null && ws.myPermissionType !== 'READ'
   )[0];
