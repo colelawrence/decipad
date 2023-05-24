7ceef0a8fc291e10e3f6959cd9fb54b4512b74f5
Nuno Job <dscape@users.noreply.github.com>
2023-05-24 11:20:09 +0100

    e2e(gallery): Makes it so adding tests to gallery is simple (#3635)

--- apps/e2e/tests/checks-gallery-notebooks.spec.ts
+++ apps/e2e/tests/checks-gallery-notebooks.spec.ts
@@ -1,4 +1,6 @@
 import { BrowserContext, expect, Page, test } from '@playwright/test';
+import fs from 'fs/promises';
+import path from 'path';
 import {
   getPadName,
   navigateToNotebook,
@@ -6,49 +8,27 @@ import {
   waitForEditorToLoad,
 } from '../utils/page/Editor';
 import { createWorkspace, importNotebook, Timeouts } from '../utils/src';
-import notebookSource from '../__fixtures__/gallery/performance-summary-letter.json';
 
-test.describe('Loading and snapshot of notebook with charts', () => {
-  test.describe.configure({ mode: 'serial' });
-
-  let notebookId: string;
-  let workspaceId: string;
-  let page: Page;
-  let context: BrowserContext;
-  test.beforeAll(async ({ browser }) => {
-    page = await browser.newPage();
-    context = await page.context();
-
-    await setUp(
-      { page, context },
-      {
-        createAndNavigateToNewPad: false,
-      }
-    );
-    workspaceId = await createWorkspace(page);
-    notebookId = await importNotebook(
-      workspaceId,
-      Buffer.from(JSON.stringify(notebookSource), 'utf-8').toString('base64'),
-      page
-    );
-  });
-
-  test.afterAll(async () => {
-    await page.close();
-  });
-
-  test('navigates to notebook and loads it', async () => {
+const navigateToNotebookPageStep = (
+  page: Page,
+  notebookId: string,
+  notebookTitle?: string
+) =>
+  test.step(`Navigates to ${notebookId} (${notebookTitle}) notebook and loads it`, async () => {
     await navigateToNotebook(page, notebookId);
     // some time for the notebook to render
     await waitForEditorToLoad(page);
-    expect(await getPadName(page)).toBe(
-      '[Template] Performance summary letter'
-    );
+    expect(await getPadName(page)).toContain('[Template]');
     // eslint-disable-next-line playwright/no-wait-for-timeout
     await page.waitForTimeout(Timeouts.chartsDelay);
   });
 
-  test('checks for errors', async () => {
+const checkForErrorsStep = (
+  page: Page,
+  notebookId: string,
+  notebookTitle?: string
+) =>
+  test.step(`Check errors on ${notebookId} (${notebookTitle})`, async () => {
     // note: This will stop picking up errors if we change the icon of loading
     const hasMagicErrors = await page.locator(
       'p span >svg title:has-text("Loading")'
@@ -74,5 +54,54 @@ test.describe('Loading and snapshot of notebook with charts', () => {
 
     // See if it has error blocks
     expect.soft(bEC, `broken blocks`).toBe(0);
+  });
+
+test.describe('Check on gallery of templates', () => {
+  test.describe.configure({ mode: 'serial' });
+
+  let workspaceId: string;
+  let page: Page;
+  let context: BrowserContext;
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+    context = await page.context();
+
+    await setUp(
+      { page, context },
+      {
+        createAndNavigateToNewPad: false,
+      }
+    );
+    workspaceId = await createWorkspace(page);
+  });
+
+  test.afterAll(async () => {
+    await page.close();
+  });
+
+  test('checks all the files', async () => {
+    const files = await fs.readdir(
+      path.join(__dirname, '..', '__fixtures__', 'gallery')
+    );
+    const jsonFiles = files.filter((file) => file.endsWith('.json'));
+
+    expect(jsonFiles.length).toBeGreaterThan(0);
+    for (const file of jsonFiles) {
+      const filePath = path.join(
+        __dirname,
+        '..',
+        '__fixtures__',
+        'gallery',
+        file
+      );
+      const fileContent = await fs.readFile(filePath, 'utf8');
+      const notebookId = await importNotebook(
+        workspaceId,
+        Buffer.from(fileContent, 'utf-8').toString('base64'),
+        page
+      );
+      await navigateToNotebookPageStep(page, notebookId, file);
+      await checkForErrorsStep(page, notebookId, file);
+    }
   });
 });
