2eceea2549bc2462da52788bfd5c5b03c9f2e500
Pedro Teixeira <i@pgte.me>
2023-05-25 11:26:50 +0100

    fixes column materialization in data views (#3649)

--- libs/editor-data-view/src/utils/materializeColumn.ts
+++ libs/editor-data-view/src/utils/materializeColumn.ts
@@ -1,4 +1,4 @@
-import { all } from '@decipad/generator-utils';
+import { all, slice } from '@decipad/generator-utils';
 import { Interpreter } from '@decipad/computer';
 
 export type AnyImmaterializedColumn = {
@@ -9,16 +9,28 @@ export type AnyMaterializedColumn = {
   value: Interpreter.OneResult[];
 };
 
+export type AnyColumn = AnyImmaterializedColumn | AnyMaterializedColumn;
+
 export type ResultingMaterializedColumn<T> = Omit<T, 'value'> &
   AnyMaterializedColumn;
 
+const MAX_COLUMN_LENGTH = 100_000;
+
 export const materializeColumn = async <
-  T extends AnyImmaterializedColumn,
+  T extends AnyColumn,
   R = ResultingMaterializedColumn<T>
 >(
   column: T
 ): Promise<R> => {
-  const value = await all(column.value());
+  if (Array.isArray(column.value)) {
+    return column as unknown as R;
+  }
+  const value = await all(slice(column.value(), 0, MAX_COLUMN_LENGTH + 1));
+  if (value.length > MAX_COLUMN_LENGTH) {
+    throw new Error(
+      `Maximum column length of ${MAX_COLUMN_LENGTH} reached. Bailing.`
+    );
+  }
   return {
     ...column,
     value,
