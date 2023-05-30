c32f561a0285b75be24165bdd7b126af71dec009
Ryan C <87027981+ryangchung@users.noreply.github.com>
2023-05-30 07:59:54 -0400

    test(e2e): Tests code formula tables (#3655)
    * added formula table test
    
    * updated test name
    
    ---------
    
    Co-authored-by: Ryan <ry@n1n.co>
    Co-authored-by: Nuno Job <dscape@users.noreply.github.com>

---
+++ apps/e2e/tests/tables-formula.spec.ts
@@ -0,0 +1,119 @@
+import { BrowserContext, expect, Page, test } from '@playwright/test';
+import notebookSource from '../__fixtures__/006-notebook-formula-tables.json';
+import {
+  navigateToNotebook,
+  setUp,
+  waitForEditorToLoad,
+} from '../utils/page/Editor';
+import { createWorkspace, importNotebook } from '../utils/src';
+
+export const typeTest = (currentPage: Page, input: string) =>
+  test.step('Trying to add text to formulas', async () => {
+    await currentPage.getByTestId('code-line').type(input);
+  });
+
+test.describe('Testing tables created from formulas', () => {
+  test.describe.configure({ mode: 'serial' });
+
+  let notebookId: string;
+  let workspaceId: string;
+  let page: Page;
+  let context: BrowserContext;
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+    context = await page.context();
+
+    await setUp(
+      { page, context },
+      {
+        createAndNavigateToNewPad: false,
+      }
+    );
+    workspaceId = await createWorkspace(page);
+    notebookId = await importNotebook(
+      workspaceId,
+      Buffer.from(JSON.stringify(notebookSource), 'utf-8').toString('base64'),
+      page
+    );
+  });
+
+  test.afterAll(async () => {
+    await page.close();
+  });
+
+  test('Waiting for editor to be ready', async () => {
+    await navigateToNotebook(page, notebookId);
+    await waitForEditorToLoad(page);
+  });
+
+  test('Filling in the code and testing that the numbers are displayed properly', async () => {
+    // This test will fail if topological sort is not working properly
+    // Failing behavior:
+    // +------+--------+
+    // | Name | Values |
+    // +------+--------+
+    // | A    | 1      |
+    // +------+--------+
+    // | B    | 2      |
+    // +------+--------+
+    // | C    | 3      |
+    // +------+--------+
+    // | D    | 1      |
+    // +------+--------+
+
+    await page.getByTestId('code-line').click();
+    await page
+      .getByTestId('code-line')
+      .fill(
+        'Table = {Name = ["A", "B", "C", "D"] \n Values = [Unnamed1, Unnamed2, Unnamed3, Unnamed4]}'
+      );
+    await expect(
+      page
+        .getByTestId('editor-table')
+        .getByTestId('text-result:A')
+        .getByText('A')
+    ).toBeVisible();
+    await expect(
+      page
+        .getByTestId('editor-table')
+        .getByTestId('text-result:B')
+        .getByText('B')
+    ).toBeVisible();
+    await expect(
+      page
+        .getByTestId('editor-table')
+        .getByTestId('text-result:C')
+        .getByText('C')
+    ).toBeVisible();
+    await expect(
+      page
+        .getByTestId('editor-table')
+        .getByTestId('text-result:D')
+        .getByText('D')
+    ).toBeVisible();
+    await expect(
+      page
+        .getByTestId('editor-table')
+        .getByTestId('number-result:1')
+        .getByText('1')
+    ).toBeVisible();
+    await expect(
+      page
+        .getByTestId('editor-table')
+        .getByTestId('number-result:2')
+        .getByText('2')
+    ).toBeVisible();
+    await expect(
+      page
+        .getByTestId('editor-table')
+        .getByTestId('number-result:3')
+        .getByText('3')
+    ).toBeVisible();
+    await expect(
+      page
+        .getByTestId('editor-table')
+        .getByTestId('number-result:4')
+        .getByText('4')
+    ).toBeVisible();
+  });
+});
