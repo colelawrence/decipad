57cb814c4d0ef6cbf6de62d8ed73d5cd23023c08
Ryan C <87027981+ryangchung@users.noreply.github.com>
2023-06-12 08:56:17 -0400

    ci(playwright): Artifact Uploads to Github Actions (#3680)
    * attempt to add artifact uploads to github actions
    
    * on failure upload, reverted to existing script
    
    * always uploading artifacts to make sure it works
    
    * changed config to allow output to test artifacts
    
    * seeing if another directory works
    
    * trying another directory
    
    * another attempt
    
    * debugging
    
    * debugging where the files are
    
    * explicitly defining that a report should exist
    
    * uncommenting accidentally commented yml
    
    * adding html reporter and fixing conflicts
    
    * trying to find playwright-report
    
    * debug
    
    * found the folder
    
    * remove debug statements
    
    ---------
    
    Co-authored-by: Ryan <ry@n1n.co>

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -25,7 +25,7 @@ services_setup
 echo "running E2E tests and snapshots..."
 cd apps/e2e
 if [ -n "${CI:-}" ]; then
-  npx percy exec -v -- npx playwright test --retries=3 --reporter line
+  npx percy exec -v -- npx playwright test --retries=3 --reporter line --reporter=html
 else
   npx playwright test --retries=3 $@
 fi
