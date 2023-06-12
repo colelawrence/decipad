18ef266733548906d574cf9999ad0ab07b3e3e92
Ryan C <87027981+ryangchung@users.noreply.github.com>
2023-06-12 13:01:49 -0400

    reproduced bug using 2x double click (#3752)
    Co-authored-by: Ryan <ry@n1n.co>

---
+++ apps/e2e/tests/triple-click-crash.spec.ts
@@ -0,0 +1,35 @@
+import { BrowserContext, Page, test, expect } from '@playwright/test';
+import { setUp } from '../utils/page/Editor';
+import { createWorkspace } from '../utils/src';
+
+test.describe('Makes sure triple click or more does not crash app', () => {
+  test.describe.configure({ mode: 'serial' });
+
+  let page: Page;
+  let context: BrowserContext;
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+    context = await page.context();
+
+    await setUp(
+      { page, context },
+      {
+        createAndNavigateToNewPad: true,
+      }
+    );
+
+    await createWorkspace(page);
+  });
+
+  test.afterAll(async () => {
+    await page.close();
+  });
+
+  test('Simulation of triple click or more through two double clicks', async () => {
+    await page.getByTestId('paragraph-content').last().fill('test test test');
+    await page.getByText('test test test').dblclick();
+    await page.getByText('test test test').dblclick();
+    await page.keyboard.press('Backspace');
+    await expect(page.getByText('Contact support')).toBeHidden();
+  });
+});
