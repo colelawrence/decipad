90d62012aaa163ff0b2ea8cf3be00ea6210cc81a
Ryan C <87027981+ryangchung@users.noreply.github.com>
2023-06-16 03:22:17 -0400

    allowing 2 workers (#3807)
    Co-authored-by: Ryan <ry@n1n.co>

--- apps/e2e/playwright.config.ts
+++ apps/e2e/playwright.config.ts
@@ -33,8 +33,8 @@ const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Using two workers on CI. */
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'github' : 'html',
 
