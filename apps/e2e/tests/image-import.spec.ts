771672dec016cb7e4c42dd4352109e5f4fe243e4
Ryan C <87027981+ryangchung@users.noreply.github.com>
2023-06-29 15:25:14 -0400

    Added image upload test (#3950)
    Co-authored-by: Ryan <ry@n1n.co>

---
+++ apps/e2e/tests/image-import.spec.ts
@@ -0,0 +1,42 @@
+// <reference lib="dom"/>
+import { BrowserContext, Page, test, expect } from '@playwright/test';
+import { setUp } from '../utils/page/Editor';
+import { Timeouts, createWorkspace } from '../utils/src';
+
+test.describe('Make sure importing images work', () => {
+  test.describe.configure({ mode: 'serial' });
+
+  let page: Page;
+  let context: BrowserContext;
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+    context = await page.context();
+
+    await setUp(
+      { page, context },
+      {
+        createAndNavigateToNewPad: true,
+      }
+    );
+
+    await createWorkspace(page);
+  });
+
+  test.afterAll(async () => {
+    await page.close();
+  });
+
+  test('Importing image through file explorer', async () => {
+    await page.getByTestId('paragraph-content').last().fill('/');
+    // eslint-disable-next-line playwright/no-wait-for-timeout
+    await page.waitForTimeout(Timeouts.menuOpenDelay);
+    await page.getByTestId('menu-item-upload-image').first().click();
+    const fileChooserPromise = page.waitForEvent('filechooser');
+    await page.getByText('Choose file').click();
+    const fileChooser = await fileChooserPromise;
+    await fileChooser.setFiles('./__fixtures__/images/download.png');
+    // eslint-disable-next-line playwright/no-wait-for-timeout
+    await page.waitForTimeout(Timeouts.computerDelay);
+    await expect(page.getByRole('figure').locator('img')).toBeVisible();
+  });
+});
