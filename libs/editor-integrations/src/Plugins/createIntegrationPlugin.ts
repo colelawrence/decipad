2c557b5fc6a765f58ff0e446c1001fb3a027d198
John Costa <69081170+JohnCosta27@users.noreply.github.com>
2023-07-07 16:39:57 +0100

    FIX: Adding interceptor to block events (specifically enter) (#3992)

--- libs/editor-integrations/src/Plugins/createIntegrationPlugin.ts
+++ libs/editor-integrations/src/Plugins/createIntegrationPlugin.ts
@@ -2,10 +2,21 @@ import {
   ELEMENT_INTEGRATION,
   createTPluginFactory,
 } from '@decipad/editor-types';
+import { createEventInterceptorPluginFactory } from '@decipad/editor-plugins';
 import { IntegrationBlock } from '../Integration/IntegrationBlock';
 
 export const createIntegrationPlugin = createTPluginFactory({
   key: ELEMENT_INTEGRATION,
   isElement: true,
   component: IntegrationBlock,
+  plugins: [
+    // Intercept events
+    createEventInterceptorPluginFactory({
+      name: 'INTERCEPTOR_INTEGRATION_BLOCK_PLUGIN',
+      elementTypes: [ELEMENT_INTEGRATION],
+      interceptor: () => {
+        return true;
+      },
+    })(),
+  ],
 });
