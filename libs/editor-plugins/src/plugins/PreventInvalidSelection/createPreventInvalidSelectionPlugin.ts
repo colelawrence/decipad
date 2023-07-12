c9c4440ff9933fbac495457a991ef3e1bae52fc7
Pedro Teixeira <i@pgte.me>
2023-07-12 13:01:38 +0100

    fix(editor): try to prevent selection from erroring (#4024)

--- libs/editor-plugins/src/plugins/PreventInvalidSelection/createPreventInvalidSelectionPlugin.ts
+++ libs/editor-plugins/src/plugins/PreventInvalidSelection/createPreventInvalidSelectionPlugin.ts
@@ -1,5 +1,12 @@
 import { MyPlatePlugin } from '@decipad/editor-types';
-import { hasNode } from '@udecode/plate';
+import { hasPoint } from '@decipad/editor-utils';
+import {
+  hasNode,
+  isText,
+  getNode,
+  TOperation,
+  getStartPoint,
+} from '@udecode/plate';
 import { Point } from 'slate';
 
 export const createPreventInvalidSelectionPlugin = (): MyPlatePlugin => ({
@@ -7,22 +14,55 @@ export const createPreventInvalidSelectionPlugin = (): MyPlatePlugin => ({
   withOverrides: (editor) => {
     const { apply } = editor;
 
-    const hasPathPoint = (point?: Point): boolean =>
-      point ? hasNode(editor, point.path) : true;
+    const isValidPoint = (point?: Point): point is Point =>
+      point
+        ? hasNode(editor, point.path) && isText(getNode(editor, point.path))
+        : true;
 
-    // eslint-disable-next-line no-param-reassign
-    editor.apply = (op) => {
+    const fixPoint = (point: Point): Point | undefined => {
+      if (!hasPoint(editor, point)) {
+        return;
+      }
+      const start = getStartPoint(editor, point.path);
+      return {
+        ...start,
+        offset: point.offset,
+      };
+    };
+
+    const tryApply = (op: TOperation, attempts = 0): void => {
       if (op.type === 'set_selection') {
+        if (attempts > 1) {
+          return;
+        }
         const newSelection = op.newProperties;
-        const paths = newSelection
+        const points = newSelection
           ? [newSelection.anchor, newSelection.focus]
           : [];
-        if (!paths.every(hasPathPoint)) {
+        if (!points.every(isValidPoint)) {
+          const newPoints = (points as Point[]).map(fixPoint);
+          if (newPoints.every(Boolean)) {
+            return tryApply(
+              {
+                ...op,
+                newProperties: {
+                  ...op.newProperties,
+                  anchor: (newPoints as [Point, Point])[0],
+                  focus: (newPoints as [Point, Point])[1],
+                },
+              },
+              attempts + 1
+            );
+          }
+          console.warn('prevented set_selection', op);
           return;
         }
       }
       apply(op);
     };
+
+    // eslint-disable-next-line no-param-reassign
+    editor.apply = tryApply;
 
     return editor;
   },
