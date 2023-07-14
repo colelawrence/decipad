ba53e09dd3c753dae48b6a4a70d87ae157b8fa2b
Ryan C <87027981+ryangchung@users.noreply.github.com>
2023-07-14 06:22:21 -0400

    Revert "ci(e2e): Fix Percy Snapshot Upload Issues (#3958)" (#4052)
    This reverts commit cc05cc9d34eadf32890a5e6a251768d238e18020.

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -26,7 +26,7 @@ services_setup
 echo "running E2E tests and snapshots..."
 cd apps/e2e
 if [ -n "${CI:-}" ]; then
-  npx percy exec -v -- npx playwright test --reporter line --reporter=html
+  npx percy exec -v -- npx playwright test --retries=3 --reporter line --reporter=html --shard=$1/$2
 else
   npx playwright test --retries=3 $@
 fi
