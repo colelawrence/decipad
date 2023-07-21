17f04b70c00e595770fdd0517b3d8e5fad3e6bf0
Pedro Teixeira <i@pgte.me>
2023-07-21 11:29:54 +0100

    do not crash when saving selection errors (#4098)
    Co-authored-by: Nuno Job <dscape@users.noreply.github.com>

--- libs/editor-plugins/src/plugins/PersistSelection/saveSelection.ts
+++ libs/editor-plugins/src/plugins/PersistSelection/saveSelection.ts
@@ -1,11 +1,20 @@
 import stringify from 'json-stringify-safe';
 import { BaseRange } from 'slate';
 
+let showedError = false;
+
 export const saveSelection = (
   storageKey: string,
   selection?: Partial<BaseRange> | null
 ) => {
   if (selection) {
-    global.localStorage.setItem(storageKey, stringify(selection));
+    try {
+      global.localStorage.setItem(storageKey, stringify(selection));
+    } catch (err) {
+      if (!showedError) {
+        showedError = true;
+        console.error('Error saving selection', err);
+      }
+    }
   }
 };
