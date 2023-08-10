d627264854f977305cb5d13084268c3636995768
Pedro Teixeira <i@pgte.me>
2023-08-10 18:52:13 +0100

    fix(websockets): does not accept JSON messages through websocket (#4236)

--- libs/y-websocket/src/isValidMessage.ts
+++ libs/y-websocket/src/isValidMessage.ts
@@ -2,9 +2,15 @@ import { Buffer } from 'buffer';
 
 export const isValidMessage = (m: Uint8Array | string): boolean => {
   const s = m instanceof Uint8Array ? Buffer.from(m).toString('base64') : m;
+  if (s.length > 10_000) {
+    // we don't even want to parse this
+    return true;
+  }
   try {
+    // if this is a JSON message with an error prop it means there was a server-side problem
+    // let's ignore it
     const json = JSON.parse(s);
-    return !('message' in json) || json.message !== 'Internal server error';
+    return !('message' in json);
   } catch (err) {
     // not JSON, so we're good
     return true;
