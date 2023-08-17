39dbc05e1d619f918d39ec06c0db22884dabc72c
Pedro Teixeira <i@pgte.me>
2023-08-17 14:46:59 +0100

    some debugging

--- libs/y-websocket/src/printReceivedMessage.ts
+++ libs/y-websocket/src/printReceivedMessage.ts
@@ -10,6 +10,6 @@ export const printReceivedMessage = (m: string | Uint8Array) => {
   const s = m instanceof Uint8Array ? Buffer.from(m).toString('base64') : m;
   if (!isTesting) {
     // eslint-disable-next-line no-console
-    console.debug(`%creceived on WS: ${s}`, logsColor);
+    console.debug(`%creceived on WS: ${s} (${typeof s})`, logsColor);
   }
 };
