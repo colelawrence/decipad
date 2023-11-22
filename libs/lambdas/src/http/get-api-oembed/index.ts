d8134f8fca8d6f5a9bd0a35fca75ce30ce2b2aca
Pedro Teixeira <i@pgte.me>
2023-11-22 12:39:43 +0000

    fixes oembed error (#4890)

--- libs/lambdas/src/http/get-api-oembed/index.ts
+++ libs/lambdas/src/http/get-api-oembed/index.ts
@@ -1,20 +1,53 @@
 import { APIGatewayProxyResultV2 as HttpResponse } from 'aws-lambda';
+import { matchPath } from 'react-router-dom';
+import { notAcceptable, notFound } from '@hapi/boom';
 import handle from '../handle';
+import { tables } from '@decipad/tables';
+import { app } from '@decipad/backend-config';
+import { expectAuthorized } from '@decipad/services/authorization';
+import { getAuthenticatedUser } from '../../../../services/src/authentication/authenticate';
 
 export const handler = handle(async (req): Promise<HttpResponse> => {
-  if (!req.queryStringParameters || !req.queryStringParameters.url) {
-    return {
-      statusCode: 400,
-      body: 'Missing url',
-    };
+  const { url } = req.queryStringParameters ?? {};
+  if (!url) {
+    throw notAcceptable('need url parameter');
   }
 
-  const title =
-    decodeURIComponent(req.queryStringParameters.url)
-      .split('/n/')[1]
-      .split(':')[0]
-      .replaceAll('-', ' ')
-      .trim() || 'Decipad - Make sense of numbers';
+  let urlUrl: URL;
+  try {
+    urlUrl = new URL(url);
+  } catch (err) {
+    throw notAcceptable((err as Error).message);
+  }
+
+  const match = matchPath('/n/:notebookId', urlUrl.pathname);
+  if (!match) {
+    throw notAcceptable('invalid URL');
+  }
+
+  const { notebookId } = match.params;
+  if (!notebookId) {
+    throw notAcceptable('invalid notebook id');
+  }
+  const notebookIdParts = notebookId.split(':');
+  const actualNotebookId = notebookIdParts[1] ?? notebookIdParts[0];
+  if (!actualNotebookId) {
+    throw notAcceptable('invalid notebook id');
+  }
+
+  const data = await tables();
+  const notebook = await data.pads.get({ id: actualNotebookId });
+  if (!notebook) {
+    throw notFound('notebook not found');
+  }
+
+  await expectAuthorized({
+    resource: `/pads/${notebook.id}`,
+    minimumPermissionType: 'READ',
+    user: await getAuthenticatedUser(req),
+  });
+
+  const title = notebook.name;
 
   return {
     statusCode: 200,
@@ -26,7 +59,7 @@ export const handler = handle(async (req): Promise<HttpResponse> => {
       version: '1.0',
       title,
       provider_name: 'Decipad',
-      provider_url: 'https://decipad.com',
+      provider_url: app().urlBase,
     }),
   };
 });
