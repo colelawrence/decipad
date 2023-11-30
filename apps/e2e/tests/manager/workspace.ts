8dfd33d129021bc0280d0094f7f1aa2c4b04f2b2
IzStriker <44909896+IzStriker@users.noreply.github.com>
2023-11-30 15:47:28 +0000

    Ig/refactor workspace tests (#4942)
    * fix(notebook): Removes permanently delete notebook from topbar
    
    * feat: remove delete and archive tests from dashboard
    
    * feat: change put back to unarchive
    
    * fix: fix typecheck errors
    
    * fix: on show delete button from workspace
    
    * fix: make test selector more specific
    
    * chore: save progress
    
    * refactor: split tests
    
    * refactor: rework workspace tests
    
    ---------
    
    Co-authored-by: Nuno Job <nunojobpinto@gmail.com>

--- apps/e2e/tests/manager/workspace.ts
+++ apps/e2e/tests/manager/workspace.ts
@@ -1,4 +1,17 @@
-import { type Locator, type Page, expect } from '@playwright/test';
+import {
+  type Locator,
+  type Page,
+  expect,
+  type ElementHandle,
+} from '@playwright/test';
+
+interface Pad {
+  anchor: ElementHandle;
+  name: string;
+  href: string | null;
+}
+
+type PadList = Pad[];
 
 export class Workspace {
   readonly page: Page;
@@ -7,6 +20,7 @@ export class Workspace {
   readonly workspaceHeroName: Locator;
   baseWorkspaceID: string;
   readonly notebookLabel: Locator;
+  readonly modalCloseButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -17,6 +31,7 @@ export class Workspace {
     this.workspaceHeroName = this.page.getByTestId('workspace-hero-title');
     this.baseWorkspaceID = '';
     this.notebookLabel = page.getByTestId('notebook-section-tag');
+    this.modalCloseButton = page.getByTestId('closable-modal');
   }
 
   /**
@@ -66,5 +81,223 @@ export class Workspace {
         hasText: label,
       })
     ).toBeVisible();
+  }
+
+  /**
+   * Create a new section in the workspace
+   *
+   * @param {string} name - Name of the section
+   * @param {number} [options.colourIndex=-1] - Colour index of the section
+   * @param {boolean} [options.create=true] - Whether to create the section or not. If not created modal will still be open after execution.
+   *
+   * **Usage**
+   * ```js
+   * await workspace.newSection('Section Name');
+   *
+   * // with colour
+   * await workspace.newSection('Section Name', { colourIndex: 2 });
+   *
+   * // without creating
+   * await workspace.newSection('Section Name', { create: false });
+   * ```
+   */
+  async newSection(
+    name: string,
+    options: { colourIndex?: number; create?: boolean } = {
+      colourIndex: -1,
+      create: true,
+    }
+  ) {
+    await this.page.getByTestId('new-section-button').click();
+    await this.page.getByPlaceholder('My section').fill(name);
+    if (options.colourIndex && options.colourIndex > 1) {
+      await this.page
+        .getByTestId('color-section-button')
+        .nth(options.colourIndex)
+        .click();
+    }
+    if (options.create === false) return;
+    await this.page.getByRole('button', { name: 'Create Section' }).click();
+    await expect(this.page.getByText(name)).toBeVisible();
+  }
+
+  /**
+   * Search for a notebook in workspace
+   *
+   * @param {string} [value=23] - value to be searched
+   *
+   * **Usage**
+   * ```js
+   * await workspace.searchForNotebook('Notebook Name');
+   * ```
+   */
+  async searchForNotebook(value: string) {
+    await this.page.getByTestId('search-bar').click();
+    await this.page.getByTestId('search-bar').pressSequentially(value);
+  }
+
+  /**
+   * Move notebook from current section to another section
+   * @param {string} notebookName - Name of the notebook to be moved
+   * @param {string} sectionName - Name of the section to move the notebook to
+   *
+   * **Usage**
+   * ```js
+   * await workspace.moveNotebookToSection('Notebook Name', 'Section Name');
+   * ```
+   */
+  async moveNotebookToSection(notebookName: string, sectionName: string) {
+    await this.page
+      .getByTestId('notebook-list-item')
+      .getByText(notebookName)
+      .hover();
+    await this.page.mouse.down();
+    await this.page.getByText(sectionName).hover();
+    await this.page.mouse.up();
+  }
+
+  /**
+   * Change the active section in workspace
+   * @param {string} sectionName - Name of the section to be selected
+   */
+  async selectSection(sectionName: string) {
+    await this.page
+      .getByTestId('navigation-list-item')
+      .getByText(sectionName)
+      .click();
+  }
+
+  /**
+   * Get the list of notepad in the current workspace
+   * @param {boolean} [sortByName=false] return a sorted list of notepad by name
+   */
+  async getPadList(sortByName = false): Promise<PadList> {
+    // wait for notebooks to show up
+    await this.page.waitForSelector('//main//li//a//strong');
+
+    // eslint-disable-next-line playwright/no-element-handle
+    const names = await this.page.$$('//main//li//a//strong');
+    const pads: PadList = [];
+    for (const name of names) {
+      const anchor = (await name.evaluateHandle(
+        (elem) => elem.closest('a')!
+      )) as ElementHandle;
+      pads.push({
+        anchor,
+        name: (await name.textContent()) ?? '',
+        href: await anchor.getAttribute('href'),
+      });
+    }
+
+    if (sortByName) {
+      return pads.sort((a, b) => a.name.localeCompare(b.name));
+    }
+    return pads;
+  }
+
+  /**
+   * Remove pad from current workspace
+   * @param {number} [index=0] index of the notepad to be removed
+   */
+  async removePad(index = 0) {
+    await this.page.click(this.ellipsisSelector(index));
+    const removeButton = await this.page.waitForSelector(
+      'div[role="menuitem"] span:has-text("Archive")'
+    );
+    await Promise.all([
+      this.page.waitForRequest('/graphql'),
+      removeButton.click(),
+    ]);
+  }
+
+  ellipsisSelector(n: number): string {
+    return `//main//li >> nth=${n} >> div[type=button] svg`;
+  }
+
+  /**
+   * Duplicate pad from current workspace
+   * @param {number} [index=0] index of notepad to be duplicated
+   * @param {string} [workspace=''] name of the workspace to copy the notepad to
+   */
+  async duplicatePad(index = 0, workspace = '') {
+    await this.page.click(this.ellipsisSelector(index));
+    await this.page.getByText('Duplicate').click();
+    // accounts with multiple workspaces have an extra menu to select where to copy
+    if (workspace !== '') {
+      const selectDuplicateWorkspace = this.page
+        .locator(`div[role="menuitem"] span:has-text("${workspace}")`)
+        .first();
+      await Promise.all([
+        this.page.waitForRequest('/graphql'),
+        selectDuplicateWorkspace.click(),
+      ]);
+    }
+  }
+
+  /**
+   * Archive notepad, if index not specified first notepad will be archived
+   * @param {number} [index=0] index of notepad to be archived
+   */
+  async archivePad(index = 0) {
+    await this.page.click(this.ellipsisSelector(index));
+    await this.page.click('div[role="menuitem"] span:has-text("Archive")');
+  }
+
+  /**
+   * Open archive tab in workspace
+   */
+  async openArchive() {
+    await this.page.click('aside nav > ul > li a span:has-text("Archived")');
+  }
+
+  /**
+   * Delete notepad, should be called when in archive, if index not specified first notepad will be deleted
+   * @param {number} [index=0] index of notepad to be deleted
+   */
+  async deleteNotepad(index = 0) {
+    await this.page.click(this.ellipsisSelector(index));
+    await this.page.click('div[role="menuitem"] span:has-text("Delete")');
+  }
+
+  /**
+   * Open account settings modal
+   */
+  async openAccountSettings() {
+    await this.page.getByTestId('account-settings-button').click();
+  }
+
+  /**
+   * close and save account settings modal
+   */
+  async closeAccountSettings() {
+    await this.page.getByTestId('btn-create-modal').click();
+  }
+
+  /**
+   * update values in account settings
+   * @param {string} [name=null] value to update the name field
+   * @param {string} [username=null] value to update the username field
+   */
+  async updateAccountSettings({
+    name = null as null | string,
+    username = null as null | string,
+  }) {
+    await this.openAccountSettings();
+    if (name) {
+      await this.page.getByTestId('user-name').fill(name);
+    }
+
+    if (username) {
+      await this.page.getByTestId('user-username').fill(username);
+    }
+
+    await this.closeAccountSettings();
+  }
+
+  /**
+   * Click new pad button
+   */
+  async clickNewPadButton() {
+    await this.page.getByTestId('new-notebook').click();
   }
 }
--- apps/e2e/tests/workspace.spec.ts
+++ apps/e2e/tests/workspace.spec.ts
@@ -1,40 +1,24 @@
-import { expect, test, Page, BrowserContext } from './manager/decipad-tests';
-import { setUp } from '../utils/page/Editor';
-import {
-  clickNewPadButton,
-  duplicatePad,
-  getPadList,
-  removePad,
-  ellipsisSelector,
-} from '../utils/page/Workspace';
+import { expect, test, Page } from './manager/decipad-tests';
 import { snapshot } from '../utils/src';
 
 test('Section creation', async ({ randomFreeUser }) => {
-  const { page } = randomFreeUser;
-
+  const { page, workspace } = randomFreeUser;
   await test.step('Creating a new section', async () => {
-    await page.getByTestId('new-section-button').click();
-    await page.getByPlaceholder('My section').fill('Section Creation Test 1');
-    await page.getByTestId('color-section-button').nth(2).click();
-    await page.getByRole('button', { name: 'Create Section' }).click();
-    await expect(page.getByText('Section Creation Test 1')).toBeVisible();
+    await workspace.newSection('Section Creation Test 1', { colourIndex: 2 });
   });
 
   await test.step('Cancelling a new section', async () => {
-    await page.getByTestId('new-section-button').click();
-    await page.getByPlaceholder('My section').fill('Section Creation Test 2');
-    await page.getByTestId('closable-modal').click();
+    await workspace.newSection('Section Creation Test 1', { create: false });
+    await workspace.modalCloseButton.click();
     await expect(page.getByText('Section Creation Test 2')).toBeHidden();
   });
 });
 
 test('workspace sections', async ({ randomFreeUser }) => {
-  const { page } = randomFreeUser;
+  const { page, workspace } = randomFreeUser;
 
   await test.step('new workspacsection', async () => {
-    await page.getByTestId('new-section-button').click();
-    await page.getByPlaceholder('My section').fill('Drag and Drop Test');
-    await page.getByRole('button', { name: 'Create Section' }).click();
+    await workspace.newSection('Drag and Drop Test');
 
     // Navigating back to My Notebooks
     await expect(async () => {
@@ -47,41 +31,31 @@ test('workspace sections', async ({ randomFreeUser }) => {
     await expect(async () => {
       await expect(page.getByTestId('workspace-hero-title')).toBeVisible();
     }).toPass();
-    await page.getByTestId('search-bar').click();
-    await page
-      .getByTestId('search-bar')
-      .pressSequentially('Welcome to Decipad!');
+    await workspace.searchForNotebook('Welcome to Decipad!');
 
-    // Dragging the notebook
-    await page
-      .getByTestId('notebook-list-item')
-      .getByText('Welcome to Decipad!')
-      .hover();
-    await page.mouse.down();
-    await page.getByText('Drag and Drop Test').hover();
-    await page.mouse.up();
+    await workspace.moveNotebookToSection(
+      'Welcome to Decipad!',
+      'Drag and Drop Test'
+    );
   });
 
   await test.step('Checking if the right notebook was dragged into the section', async () => {
-    await page
-      .getByTestId('navigation-list-item')
-      .getByText('Drag and Drop Test')
-      .click();
+    workspace.selectSection('Drag and Drop Test');
     await expect(
       page.getByTestId('notebook-list-item').getByText('Welcome to Decipad!')
     ).toBeVisible();
 
     // check section labels are visible with filter
-    await randomFreeUser.workspace.checkLabel('Drag and Drop Test');
+    await workspace.checkLabel('Drag and Drop Test');
 
     // check section label are persistant on reload
     await page.reload();
-    await randomFreeUser.workspace.checkLabel('Drag and Drop Test');
+    await workspace.checkLabel('Drag and Drop Test');
 
     // check standard view showcases notebook labels
     await expect(async () => {
       await page.getByTestId('my-notebooks-button').click();
-      await randomFreeUser.workspace.checkLabel('Drag and Drop Test');
+      await workspace.checkLabel('Drag and Drop Test');
     }).toPass();
   });
 
@@ -94,104 +68,88 @@ const byName = (a: { name: string }, b: { name: string }): number => {
   return a.name.localeCompare(b.name);
 };
 
-test.describe('Dashboard snapshot', () => {
-  test.describe.configure({ mode: 'serial' });
+test('should display the initial notebooks', async ({
+  randomFreeUser: { page, workspace },
+}) => {
+  const pads = await workspace.getPadList(true);
 
-  let page: Page;
-  let context: BrowserContext;
-  let localStorageValue: string | null;
+  // eslint-disable-next-line no-unused-expressions, playwright/no-conditional-in-test
+  process.env.CI || process.env.DECI_E2E
+    ? expect(pads).toMatchObject(
+        [
+          'Welcome to Decipad!',
+          'Starting a Business - Example Notebook',
+          'Weekend Trip - Example Notebook',
+        ]
+          .map((notebook) => ({ name: notebook }))
+          .sort(byName)
+      )
+    : expect(pads).toMatchObject(
+        [
+          '[Template] Capitalisation table for seed founders',
+          '[Template] Decipad Investor Update: Mar 2023',
+          '[Template] How much is Apple worth? Breaking down a DCF model.',
+          '[Template] Offer Letter',
+          '[Template] Performance summary letter',
+          '[Template] Sales Report: Monthly Pipeline Update',
+          '[Template] Shilling Founders Fund | An innovative approach to profit sharing',
+          '[Template] Sprint Capacity Calculation for Scrum Teams',
+          '[Template] Understanding stock options at an early stage startup',
+          'Everything, everywhere, all at once',
+          'Very weird loading when editing',
+          'Welcome to Decipad!',
+          'Starting a Business - Example Notebook',
+          'Weekend Trip - Example Notebook',
+        ]
+          .map((notebook) => ({ name: notebook }))
+          .sort(byName)
+      );
 
-  test.beforeAll(async ({ browser }) => {
-    page = await browser.newPage();
-    context = await page.context();
-    await setUp(
-      { page, context },
-      { createAndNavigateToNewPad: false, randomUser: true }
-    );
-  });
-
-  test.afterAll(async () => {
-    await page.close();
-  });
-
-  test('should display the initial notebooks', async () => {
-    const pads = await (await getPadList(page)).sort(byName);
-
-    // eslint-disable-next-line no-unused-expressions, playwright/no-conditional-in-test
-    process.env.CI || process.env.DECI_E2E
-      ? expect(pads).toMatchObject(
-          [
-            'Welcome to Decipad!',
-            'Starting a Business - Example Notebook',
-            'Weekend Trip - Example Notebook',
-          ]
-            .map((notebook) => ({ name: notebook }))
-            .sort(byName)
-        )
-      : expect(pads).toMatchObject(
-          [
-            '[Template] Capitalisation table for seed founders',
-            '[Template] Decipad Investor Update: Mar 2023',
-            '[Template] How much is Apple worth? Breaking down a DCF model.',
-            '[Template] Offer Letter',
-            '[Template] Performance summary letter',
-            '[Template] Sales Report: Monthly Pipeline Update',
-            '[Template] Shilling Founders Fund | An innovative approach to profit sharing',
-            '[Template] Sprint Capacity Calculation for Scrum Teams',
-            '[Template] Understanding stock options at an early stage startup',
-            'Everything, everywhere, all at once',
-            'Very weird loading when editing',
-            'Welcome to Decipad!',
-            'Starting a Business - Example Notebook',
-            'Weekend Trip - Example Notebook',
-          ]
-            .map((notebook) => ({ name: notebook }))
-            .sort(byName)
-        );
-
-    await snapshot(page as Page, 'Dashboard: Initial Notebooks');
-  });
-
-  test.use({ colorScheme: 'dark' });
-
-  test('shows workspace in dark mode mode', async () => {
-    await expect(async () => {
-      localStorageValue = await page.evaluate(() => {
-        window.localStorage.setItem('deciThemePreference', 'dark');
-        return window.localStorage.getItem('deciThemePreference');
-      });
-
-      if (localStorageValue !== null) {
-        expect(localStorageValue).toMatch('dark');
-      }
-    }).toPass();
-    await page.reload({ waitUntil: 'load' });
-    await snapshot(page as Page, 'Dashboard: Initial Notebooks Darkmode');
-  });
-
-  test.use({ colorScheme: 'light' });
-
-  test('shows workspace in light mode mode', async () => {
-    await expect(async () => {
-      localStorageValue = await page.evaluate(() => {
-        window.localStorage.setItem('deciThemePreference', 'light');
-        return localStorage.getItem('deciThemePreference');
-      });
-
-      if (localStorageValue !== null) {
-        expect(localStorageValue).toMatch('light');
-      }
-    }).toPass();
-    await page.reload({ waitUntil: 'load' });
-  });
+  await snapshot(page as Page, 'Dashboard: Initial Notebooks');
 });
 
-test('Dashboard operations', async ({ randomFreeUser }) => {
-  const { page } = randomFreeUser;
+test.use({ colorScheme: 'dark' });
+test('shows workspace in dark mode mode', async ({
+  randomFreeUser: { page },
+}) => {
+  let localStorageValue: string | null;
+  await expect(async () => {
+    localStorageValue = await page.evaluate(() => {
+      window.localStorage.setItem('deciThemePreference', 'dark');
+      return window.localStorage.getItem('deciThemePreference');
+    });
 
+    if (localStorageValue !== null) {
+      expect(localStorageValue).toMatch('dark');
+    }
+  }).toPass();
+  await page.reload({ waitUntil: 'load' });
+  await snapshot(page as Page, 'Dashboard: Initial Notebooks Darkmode');
+});
+
+test.use({ colorScheme: 'light' });
+test('shows workspace in light mode mode', async ({
+  randomFreeUser: { page },
+}) => {
+  await expect(async () => {
+    const localStorageValue = await page.evaluate(() => {
+      window.localStorage.setItem('deciThemePreference', 'light');
+      return localStorage.getItem('deciThemePreference');
+    });
+
+    if (localStorageValue !== null) {
+      expect(localStorageValue).toMatch('light');
+    }
+  }).toPass();
+  await page.reload({ waitUntil: 'load' });
+});
+
+test('Dashboard operations', async ({
+  randomFreeUser: { page, workspace },
+}) => {
   await test.step('creates a new pad and navigates to pad detail', async () => {
     await Promise.all([
-      clickNewPadButton(page),
+      workspace.clickNewPadButton(),
       page.waitForNavigation({ url: '/n/*' }),
     ]);
 
@@ -199,14 +157,14 @@ test('Dashboard operations', async ({ randomFreeUser }) => {
   });
 
   await test.step('can remove pad', async () => {
-    const padIndex = (await getPadList(page)).findIndex(
+    const padIndex = (await workspace.getPadList()).findIndex(
       (pad) => pad.name !== 'My first pad'
     );
     expect(padIndex).toBeGreaterThanOrEqual(0);
-    await removePad(page, padIndex);
+    await workspace.removePad(padIndex);
 
     await expect(async () => {
-      const pads = await getPadList(page);
+      const pads = await workspace.getPadList();
       // eslint-disable-next-line no-unused-expressions, playwright/no-conditional-in-test
       process.env.CI || process.env.DECI_E2E
         ? expect(pads).toHaveLength(3)
@@ -215,16 +173,16 @@ test('Dashboard operations', async ({ randomFreeUser }) => {
   });
 
   await test.step('can duplicate pad', async () => {
-    await duplicatePad(page);
+    await workspace.duplicatePad();
 
     await expect(async () => {
-      let pads = await getPadList(page);
+      let pads = await workspace.getPadList();
       // eslint-disable-next-line no-unused-expressions, playwright/no-conditional-in-test
       process.env.CI
         ? expect(pads).toHaveLength(4)
         : expect(pads).toHaveLength(15);
 
-      pads = await getPadList(page);
+      pads = await workspace.getPadList();
       const copyIndex = pads.findIndex((pad) =>
         pad.name?.startsWith('Copy of')
       );
@@ -233,7 +191,7 @@ test('Dashboard operations', async ({ randomFreeUser }) => {
   });
 
   await test.step('can navigate to pad detail', async () => {
-    const pads = await getPadList(page);
+    const pads = await workspace.getPadList();
     expect(pads.length).toBeGreaterThan(0);
     const pad = pads[0];
     await pad.anchor.click();
@@ -242,47 +200,34 @@ test('Dashboard operations', async ({ randomFreeUser }) => {
   });
 });
 
-test('Workspace flows', async ({ randomFreeUser }) => {
-  const { page } = randomFreeUser;
-
+test('Workspace flows', async ({ randomFreeUser: { page, workspace } }) => {
   await test.step('Archive & delete a notebook', async () => {
-    await page.click(ellipsisSelector(0)); // click first ellipsis
-    await page.click('div[role="menuitem"] span:has-text("Archive")');
-    await page.click('aside nav > ul > li a span:has-text("Archived")');
-    await page.click(ellipsisSelector(0)); // click first ellipsis
-    await page.click('div[role="menuitem"] span:has-text("Delete")');
+    await workspace.archivePad(0);
+    await workspace.openArchive();
+    await workspace.deleteNotepad(0);
     await expect(page.getByText('No documents to list')).toBeVisible();
   });
 
   await test.step('Create a workspace', async () => {
-    await page.getByTestId('workspace-selector-button').click();
-    await page.getByTestId('create-workspace-button').click();
-    await page.getByPlaceholder('Team workspace').click();
-    await page.getByPlaceholder('Team workspace').fill('Wtf');
-    await page.getByRole('button', { name: 'Create Workspace' }).click();
+    await workspace.newWorkspace('Wtf');
     await expect(page.getByTestId('workspace-hero-title')).toHaveText(
       'Welcome toWtf'
     );
   });
 
   await test.step('Update name in the account settings modal', async () => {
-    await page.getByTestId('account-settings-button').click();
-    await page.getByTestId('user-name').fill('Joe Doe');
-    await page.getByTestId('btn-create-modal').click();
-
+    await workspace.updateAccountSettings({ name: 'Joe Doe' });
     await expect(page.locator('[title="Joe Doe"]')).toHaveText('Joe Doe');
 
-    await page.getByTestId('account-settings-button').click();
-
+    await workspace.openAccountSettings();
     await expect(page.getByTestId('user-name')).toHaveValue('Joe Doe');
+    await workspace.closeAccountSettings();
   });
 
   await test.step('Update username in the account settings modal', async () => {
     const currentDate = Date.now();
-    await page.getByTestId('user-username').fill(`joedoe${currentDate}`);
-    await page.getByTestId('btn-create-modal').click();
-    await page.getByTestId('account-settings-button').click();
-
+    await workspace.updateAccountSettings({ username: `joedoe${currentDate}` });
+    await workspace.openAccountSettings();
     await expect(page.getByTestId('user-username')).toHaveValue(
       `@joedoe${currentDate}`
     );
