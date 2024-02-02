be5c157cf4ca51d32d02f02be39a4d1e7e761cd8
Pedro Teixeira <i@pgte.me>
2024-02-02 13:25:05 +0000

    fix: notebook creation: wait for all updates are flushed (#5295)

--- libs/services/src/pad-content/index.ts
+++ libs/services/src/pad-content/index.ts
@@ -5,37 +5,32 @@ import { DynamodbPersistence } from '@decipad/y-dynamodb';
 import { toSharedTypeSingular } from 'libs/slate-yjs/src/utils';
 import { EElement, Value } from '@udecode/plate-common';
 
-const createPadContent = <V extends Value = MyValue>(
+const createPadContent = async <V extends Value = MyValue>(
   path: string,
   content: EElement<V>[]
 ): Promise<void> => {
-  return new Promise((resolve, reject) => {
-    try {
-      const doc = new YDoc();
-      const provider = new DynamodbPersistence(path, doc);
-      doc.on('update', (update: Uint8Array, origin: unknown) => {
-        provider.storeUpdate(update, origin);
-      });
-
-      /** The reason we iterate, instead of simply uploading the content array,
-       * is because DynamoDB has a size limit on a singular record. So if the notebook
-       * is too large, then it would reject the update.
-       * Hence why now we iterate over each block in the notebook instead.
-       */
-      const sharedType = doc.getArray() as SharedType;
-      content.forEach((block, i) => {
-        doc.transact(() => {
-          toSharedTypeSingular(sharedType, block, i);
-        });
-      });
-
-      provider.once('flushed', () => {
-        resolve();
-      });
-    } catch (err) {
-      reject(err);
-    }
+  const doc = new YDoc();
+  const provider = new DynamodbPersistence(path, doc);
+  const updates: Array<Promise<void>> = [];
+  doc.on('update', (update: Uint8Array, origin: unknown) => {
+    updates.push(provider.storeUpdate(update, origin));
   });
+
+  /** The reason we iterate, instead of simply uploading the content array,
+   * is because DynamoDB has a size limit on a singular record. So if the notebook
+   * is too large, then it would reject the update.
+   * Hence why now we iterate over each block in the notebook instead.
+   */
+  const sharedType = doc.getArray() as SharedType;
+  content.forEach((block, i) => {
+    doc.transact(() => {
+      toSharedTypeSingular(sharedType, block, i);
+    });
+  });
+
+  await Promise.all(updates);
+  await provider.flush();
+  await Promise.all(updates); // again because an update may have happened while flushing
 };
 
 export const create = <V extends Value = MyValue>(
