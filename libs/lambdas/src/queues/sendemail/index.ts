65b58c78a835efc3dcf574f1d85c28d578223fb9
Pedro Teixeira <i@pgte.me>
2024-03-29 08:16:05 +0000

    captures email sending error (#5716)

--- libs/lambdas/src/queues/sendemail/index.ts
+++ libs/lambdas/src/queues/sendemail/index.ts
@@ -1,9 +1,10 @@
+import { APIGatewayProxyEventV2 } from 'aws-lambda';
 import { track } from '@decipad/backend-analytics';
 import templates from '@decipad/emails';
 import { sendEmail } from '@decipad/services/email';
+import { captureException } from '@decipad/backend-trace';
 import handle from '../handle';
 import { debug } from '../../debug';
-import { APIGatewayProxyEventV2 } from 'aws-lambda';
 
 // TODO restructure and use input validation to check parameters are correct for the given template, making it possible to type without any
 /* eslint-disable @typescript-eslint/no-explicit-any */
@@ -38,6 +39,7 @@ async function handleSendEmail(
     debug('email sent', params);
     await track(event, { event: 'email sent', properties: params });
   } catch (err) {
+    await captureException(err as Error);
     console.error('Error sending email', err);
   }
 }
