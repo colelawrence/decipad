d0827200de08add36ae07ee08394a5168fdd209e
Simao <SimaoDias@users.noreply.github.com>
2024-08-02 10:02:12 +0100

    feat(e2e): upload performance test results to google sheet (#6318)
    * add uploading scripts
    
    * replace hardcoded URL with secret
    
    ---------
    
    Co-authored-by: Pedro Teixeira <i@pgte.me>

---
+++ scripts/upload-performance-results.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# URL of your Google Sheets script endpoint
+URL="${UPLOAD_PERFORMANCE_RESULTS_GSHEETS_MACRO_URL}"
+
+if [[ -z "${URL:-}" ]]; then
+    echo "Must provide a UPLOAD_PERFORMANCE_RESULTS_GSHEETS_MACRO_URL env var"
+    exit 1
+fi
+
+# Path to your JSON file
+JSON_FILE="./performance-results/performance-results.json"
+
+# Send the POST request with the JSON data
+curl -X POST -H "Content-Type: application/json" -d @"${JSON_FILE}" "${URL}"
