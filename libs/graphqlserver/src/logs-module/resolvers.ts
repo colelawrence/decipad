0b3202090c22a89fb304e80b02b949c4ca321f60
Pedro Teixeira <i@pgte.me>
2024-12-06 13:30:45 +0000

    fix(backend save client logs): maximum log chunk size (#6885)

--- libs/graphqlserver/src/logs-module/resolvers.ts
+++ libs/graphqlserver/src/logs-module/resolvers.ts
@@ -1,7 +1,8 @@
 import tables, { timestamp } from '@decipad/tables';
 import { nanoid } from 'nanoid';
-import { requireUser } from '../authorization';
 import type { Resolvers } from '@decipad/graphqlserver-types';
+import chunks from 'chunk-text';
+import { requireUser } from '../authorization';
 
 const EXPIRATION_TIME_SECONDS = 30 * 24 * 60 * 60; // 30 days
 
@@ -11,6 +12,8 @@ export interface LogEntry {
   content: string;
 }
 
+const MAX_RECORD_SIZE_BYTES = 300_000;
+
 const resolvers: Resolvers = {
   Mutation: {
     async createLogs(_, args, context) {
@@ -19,17 +22,19 @@ const resolvers: Resolvers = {
       const expiresAt = timestamp() + EXPIRATION_TIME_SECONDS;
       const { input } = args;
       for (const log of input.entries) {
-        // eslint-disable-next-line no-await-in-loop
-        await data.logs.put({
-          resource: input.resource,
-          seq: `${Date.now()}:${nanoid()}`,
-          id: nanoid(),
-          createdAt: timestamp(new Date(log.createdAt)),
-          source: log.source,
-          user_id: user.id,
-          content: log.content,
-          expiresAt,
-        });
+        for (const chunk of chunks(log.content, MAX_RECORD_SIZE_BYTES)) {
+          // eslint-disable-next-line no-await-in-loop
+          await data.logs.put({
+            resource: input.resource,
+            seq: `${Date.now()}:${nanoid()}`,
+            id: nanoid(),
+            createdAt: timestamp(new Date(log.createdAt)),
+            source: log.source,
+            user_id: user.id,
+            content: chunk,
+            expiresAt,
+          });
+        }
       }
 
       return true;
