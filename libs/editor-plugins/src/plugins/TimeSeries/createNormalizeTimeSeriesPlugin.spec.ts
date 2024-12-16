13d100d91322595417ec149850d47f526c21ec7e
Bruno Lourenco <brunnolou@B-M4-Pro.lan>
2024-12-16 12:00:22 +0000

    chore(time-series): remove unused migration test

--- libs/editor-plugins/src/plugins/TimeSeries/createNormalizeTimeSeriesPlugin.spec.ts
+++ libs/editor-plugins/src/plugins/TimeSeries/createNormalizeTimeSeriesPlugin.spec.ts
@@ -1,22 +1,10 @@
 import { beforeEach, expect, describe, it } from 'vitest';
-import type { TimeSeriesElement, TableElement } from '@decipad/editor-types';
 import {
   ELEMENT_TIME_SERIES,
-  ELEMENT_TIME_SERIES_TH,
-  ELEMENT_TIME_SERIES_TR,
-  ELEMENT_TABLE,
-  ELEMENT_TABLE_CAPTION,
-  ELEMENT_TABLE_COLUMN_FORMULA,
-  ELEMENT_TABLE_VARIABLE_NAME,
-  ELEMENT_TD,
-  ELEMENT_TH,
-  ELEMENT_TR,
   createMyPlateEditor,
-  ELEMENT_DATA_VIEW_CAPTION,
-  ELEMENT_DATA_VIEW_NAME,
 } from '@decipad/editor-types';
 import type { TEditor } from '@udecode/plate-common';
-import { findNode, normalizeEditor } from '@udecode/plate-common';
+import { normalizeEditor } from '@udecode/plate-common';
 import { createNormalizeTimeSeriesPlugin } from './createNormalizeTimeSeriesPlugin';
 
 describe('createNormalizeTimeSeriesPlugin', () => {
@@ -55,97 +43,5 @@ describe('createNormalizeTimeSeriesPlugin', () => {
         type: 'time-series',
       },
     ]);
-  });
-
-  it('migrates from table formula IDs to table column IDs', () => {
-    editor.children = [
-      {
-        type: ELEMENT_TABLE,
-        id: 'table',
-        children: [
-          {
-            type: ELEMENT_TABLE_CAPTION,
-            id: 'caption',
-            children: [
-              {
-                type: ELEMENT_TABLE_VARIABLE_NAME,
-                id: 'tablevar',
-                children: [{ text: 'Table 1' }],
-              },
-              {
-                type: ELEMENT_TABLE_COLUMN_FORMULA,
-                id: 'bad-and-wrong-table-formula-id',
-                columnId: 'based-and-correct-th-id',
-                children: [{ text: 'Column 1' }],
-              },
-            ],
-          },
-          {
-            type: ELEMENT_TR,
-            id: 'headerrow',
-            children: [
-              {
-                type: ELEMENT_TH,
-                id: 'based-and-correct-th-id',
-                cellType: { kind: 'table-formula' },
-                children: [{ text: '1 + 1' }],
-              },
-            ],
-          },
-          {
-            type: ELEMENT_TR,
-            id: 'datarow',
-            children: [
-              {
-                type: ELEMENT_TD,
-                id: 'td1',
-                children: [{ text: 'Column 1' }],
-              },
-            ],
-          },
-        ],
-      } as TableElement,
-      {
-        type: ELEMENT_TIME_SERIES,
-        id: 'dv',
-        children: [
-          // TimeSeriesCaptionElement
-          {
-            type: ELEMENT_DATA_VIEW_CAPTION,
-            id: 'dvcaption',
-            children: [
-              {
-                type: ELEMENT_DATA_VIEW_NAME,
-                id: 'dvvar',
-                children: [{ text: 'Table 1' }],
-              },
-            ],
-          },
-          // TimeSeriesHeaderRowElement
-          {
-            type: ELEMENT_TIME_SERIES_TR,
-            id: 'dvheaderrow',
-            children: [
-              // TimeSeriesHeader
-              {
-                type: ELEMENT_TIME_SERIES_TH,
-                id: 'dvth1',
-                cellType: { kind: 'number' },
-                name: 'bad-and-wrong-table-formula-id',
-                label: 'BadWrong',
-                children: [{ text: '' }],
-              },
-            ],
-          },
-        ],
-      } as TimeSeriesElement,
-    ];
-
-    normalizeEditor(editor, { force: true });
-
-    const TimeSeriesTh = findNode(editor, { match: { id: 'dvth1' } })?.[0];
-    expect(TimeSeriesTh?.name).toMatchInlineSnapshot(
-      `"based-and-correct-th-id"`
-    );
   });
 });
