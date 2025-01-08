685803a4bc5df1b3db2290fa5f8e8f60619a779d
John Costa <69081170+JohnCosta27@users.noreply.github.com>
2025-01-08 11:34:59 +0000

    fix(percy): only using one size and not changing viewport (#6947)
    * fix: not changing view port halfway through test
    
    This is considered bad practice with snapshot testing, because it
    creates too many inconsistencies.
    
    * even betterfix
    
    * fix: adding timer back
    
    fix: linting
    
    * fix: type
    
    * feat: adding mobile widths

--- apps/e2e/utils/src/snapshot.ts
+++ apps/e2e/utils/src/snapshot.ts
@@ -1,52 +1,29 @@
 import percySnapshot from '@percy/playwright';
 import type { Page } from '@playwright/test';
 
-const PAGE_SETTLE_TIMEOUT_BEFORE_SNAPSHOT_MS = 10_000;
-
 const snapshotsTaken = new Set<string>();
 
 export const snapshot = async (
   page: Page,
   name: string,
-  options: { mobile?: boolean; midSize?: boolean } = {
-    mobile: false,
-    midSize: false,
-  }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _options?: { mobile?: boolean; midSize?: boolean }
 ): Promise<void> => {
   if (!process.env.PERCY_TOKEN || snapshotsTaken.has(name)) {
     return;
   }
   snapshotsTaken.add(name);
-  await page.waitForLoadState('load');
+
+  // Wait for the page to stabilize
   // eslint-disable-next-line playwright/no-wait-for-timeout
-  await page.waitForTimeout(PAGE_SETTLE_TIMEOUT_BEFORE_SNAPSHOT_MS);
-  await page.evaluate(() => document.fonts.ready);
+  await page.waitForTimeout(5_000);
 
-  const initialViewportSize = page.viewportSize()!;
-
-  for (const width of [options.mobile && 375, options.midSize && 768, 1380]) {
-    if (!width) continue;
-
-    await page.setViewportSize({
-      ...initialViewportSize,
-      width,
+  try {
+    await percySnapshot(page, name, {
+      widths: [1380, 768, 375],
     });
-
-    // eslint-disable-next-line playwright/no-wait-for-timeout
-    await page.waitForTimeout(PAGE_SETTLE_TIMEOUT_BEFORE_SNAPSHOT_MS);
-
-    try {
-      await percySnapshot(page, name, {
-        widths: [width],
-      });
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      console.error('error taking snapshot:', err);
-    }
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error('error taking snapshot:', err);
   }
-
-  await page.setViewportSize(initialViewportSize);
-
-  // eslint-disable-next-line playwright/no-wait-for-timeout
-  await page.waitForTimeout(PAGE_SETTLE_TIMEOUT_BEFORE_SNAPSHOT_MS);
 };
