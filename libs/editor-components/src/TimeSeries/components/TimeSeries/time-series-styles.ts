3f0ce17db298b5fc46268f459511deeab6a58969
John Costa <69081170+JohnCosta27@users.noreply.github.com>
2025-01-28 10:38:55 +0000

    fix(time-series): styles (#6992)
    # Description
    
    ## Product Pledge
    
    - [ ] (for UX reviewer) these changes meet UX standards
    - [ ] (for code reviewer) this code meets our standards for quality and maintainability
    - [ ] I have thoroughly tested this code where practical
    - [ ] I have included release notes
    - [ ] I have updated documentation (where appropriate)
    - [ ] I have dog-fooded these changes
    - [ ] I have conducted user testing
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai -->
    
    ## Summary by CodeRabbit
    
    - **Style**
    	- Updated styling for time series component, removing specific positioning and transition properties
    	- Adjusted layout of component's pseudo-element
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- libs/editor-components/src/TimeSeries/components/TimeSeries/time-series-styles.ts
+++ libs/editor-components/src/TimeSeries/components/TimeSeries/time-series-styles.ts
@@ -22,21 +22,10 @@ export const stickySecondLeftColumn = css({
 const background = cssVar('backgroundMain');
 
 export const rightAddColumnWrapper = css({
-  position: 'absolute',
-  top: 1,
-  left: '100%',
-  transition: 'left .4s',
   '::before': {
     content: '""',
 
-    position: 'absolute',
-    top: -4,
-    left: -16,
-    right: -16,
-    bottom: 0,
-
     display: 'block',
-
     background: `linear-gradient(270deg, transparent 0%, ${background} 35%, ${background} 65%, transparent 100%)`,
   },
 });
